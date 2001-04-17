bc867b3a019623938cfde46b3c51f9c16717717a
bilmes <bilmes@localhost>
2001-04-17 22:23:37 +0000

    Initial revision

---
+++ miscSupport/error.h
@@ -0,0 +1,25 @@
+/*
+    $Header$
+  
+    Simple fatal error function.
+    Jeff Bilmes <bilmes@cs.berkeley.edu>
+*/
+
+
+
+#ifndef ERROR_H
+#define ERROR_H
+
+
+#ifndef EXIT_SUCCESS
+#define EXIT_SUCCESS (0)
+#endif
+#ifndef EXIT_FAILURE
+#define EXIT_FAILURE (1)
+#endif
+
+void error(char *format, ...);
+void coredump(char *format, ...);
+void warning(char *format, ...);
+
+#endif
