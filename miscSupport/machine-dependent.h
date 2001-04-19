7a1664f8dd80af3a473262ea8dbe3eb3ff912d02
Jeff Bilmes <bilmes@users.noreply.github.com>
2001-04-19 23:59:17 +0000

    Initial revision

---
+++ miscSupport/machine-dependent.h
@@ -0,0 +1,36 @@
+/*-
+ * machine_dependent.h
+ *        Definition of a variety of machine dependnet functions, types, etc.
+ *
+ *  Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ * 
+ *  $Header$
+ * 
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle make no representations about
+ * the suitability of this software for any purpose.  It is provided
+ * "as is" without express or implied warranty.
+ *
+ */
+
+
+#ifndef MACHINE_DEPENDENT_H
+#define MACHINE_DEPENDENT_H
+
+// 32 bit int
+typedef int GMTK_int32;
+// 32 bit unsigned
+typedef unsigned int GMTK_unsigned32;
+// 32 bit pointer
+typedef int* GMTK_ptr32;
+// 64 bit pointer
+typedef double* GMTK_ptr64;
+
+
+#endif
+
