09fef267c906dc256e9b459e5d59f56f23574ff4
zweig <zweig@localhost>
2001-04-23 01:37:31 +0000

    made minor changes

--- tksrc/GMTK_GM.h
+++ tksrc/GMTK_GM.h
@@ -25,14 +25,23 @@
 
 struct GMTK_GM
 {
-    sArray<randomVariable> node;
+    sArray<randomVariable *> node;
     // This holds all the variables in the graph.
     // The topology os determined by the Parent and Child arrays associated
     // with each random variable.
 
-    sArray<randomVariable> topologicalOrder;
+    sArray<randomVariable *> topologicalOrder;
     // A topological ordering of the nodes; useful for simulation and
     // enumerative inference.
+
+    int numEquivalenceClasses;
+    // The variables in the network may be divided into equivalence classes,
+    // with the members of each equivalence class sharing the same parameters.
+
+    sArray<randomVariable *> representativeOfEquivalenceClass;
+    // All the members of an equivalence class will use the parameters 
+    // associated with this particular member. To save memory, the other
+    // member's parameter data structures simply point to this representative.
 
     void findTopologicalOrder(randomVariable *rv = NULL);
     // Guarantees that all variables at time t-1 occur before any variables
@@ -48,12 +57,21 @@ struct GMTK_GM
     // Goes over the variables in topological order and instantiates them 
     // according to the probability distribution given their parent's values.
 
-    logp enumerateProb(unsigned nodenum=0, logp p=1);
+    logpr enumerateProb(unsigned pos=0, logpr p=1);
     // A recursive function that enumerates all possible values of the 
     // hidden variables and computes the total data prob by brute force.
     // Good for verifying DP and other inference results.
 
-    logp logDataProb;
+    logpr logDataProb;
+
+    int numNodes;
+    // how many nodes are in the graph, i.e. node.len()
+
+    int sliceSize;
+    // how many nodes in a time slice
+
+    int numSlices; 
+    // how many time slices there are in the network
 };
 
 #endif
