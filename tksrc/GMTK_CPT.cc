6b99dae8e157033859d6dab1d36584c9cf245fcb
bilmes <bilmes@localhost>
2001-04-24 10:34:49 +0000

    *** empty log message ***

--- tksrc/GMTK_CPT.cc
+++ tksrc/GMTK_CPT.cc
@@ -23,110 +23,27 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <ieeefp.h>
 #include <float.h>
 #include <assert.h>
 
 #include "general.h"
 #include "error.h"
 
+#include "GMTK_CPT.h"
+
 
 VCID("$Header$");
 
+////////////////////////////////////////////////////////////////////
+//        Static Data
+////////////////////////////////////////////////////////////////////
+
+
+int CPT::warningNumParents = 50;
 
 ////////////////////////////////////////////////////////////////////
 //        General create, read, destroy routines 
 ////////////////////////////////////////////////////////////////////
-
-
-/*-
- *-----------------------------------------------------------------------
- * CPT::CPT()
- *      Constructor
- *
- * Results:
- *      Constructs the object.
- *
- * Side Effects:
- *      None so far.
- *
- *-----------------------------------------------------------------------
- */
-CPT::CPT() 
-{
-
-}
-
-
-
-/*-
- *-----------------------------------------------------------------------
- * CPT::read(is)
- *      read in a distribution from file 'is'. 
- *      The data probs are stored as doubles, but when they are read in
- *      they are converted to the log domain.
- * 
- * Results:
- *      No results.
- *
- * Side Effects:
- *      Changes the pmf member function in the object.
- *
- *-----------------------------------------------------------------------
- */
-void
-CPT::read(iDataStreamFile& is)
-{
-  is.read(rows,"Discrete1DPDF::read, rows");
-  if (rows <= 0)
-    error("Discrete1DPDF: read rows (%d) < 0 in input",rows);
-  is.read(cols,"Discrete1DPDF::read, cols");
-  if (rows <= 0)
-    error("Discrete1DPDF: read cols (%d) < 0 in input",cols);
-
-  pmf.resize(rows*cols);
-
-  logpr * ptr = pmf.ptr;
-  for (int r=0;i<rows;i++) {
-    for (int c=0;c<cols;c++) {
-      double val;
-      is.readDouble(val,"CPT::read, reading value");
-      *ptr++ = val;
-    }
-  }
-}
-
-
-
-/*-
- *-----------------------------------------------------------------------
- * CPT::write(os)
- *      write out distribution to file 'os'. 
- *      The data probs are stored as doubles not in log domain.
- * 
- * Results:
- *      No results.
- *
- * Side Effects:
- *      No effectcs other than  moving the file pointer of os.
- *
- *-----------------------------------------------------------------------
- */
-void
-CPT::write(oDataStreamFile& os)
-{
-  is.write(rows,"Discrete1DPDF::write, rows");
-  is.write(cols,"Discrete1DPDF::write, cols");
-
-  logpr * ptr = pmf.ptr;
-  for (int r=0;i<rows;i++) {
-    for (int c=0;c<cols;c++) {
-      is.writeDouble((*ptr).unlog(),"CPT::read, reading value");
-      ptr++;
-    }
-  }
-}
-
 
 
 
