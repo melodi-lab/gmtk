fc9f4fa92b6c745085096e84b1a5e8e1e1d209ae
zweig <zweig@localhost>
2001-04-24 18:55:17 +0000

    Initial revision

---
+++ tksrc/GMTK_Clique.h
@@ -0,0 +1,19 @@
+
+/* 
+ * GMTK_Clique.h
+ * The basic clique data structure.
+ *
+ * Written by Geoffrey Zweig <gzweig@us.ibm.com>
+ * 
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle make no representations about the suitability of this software 
+ * for any purpose. It is provided "as is" without express or implied warranty.
+ *
+ */ 
+
+
