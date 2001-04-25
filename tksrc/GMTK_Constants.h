c0e375c5ace3b15058c032fdb41d96272d9e121d
zweig <zweig@localhost>
2001-04-25 18:05:51 +0000

    Initial revision

---
+++ tksrc/GMTK_Constants.h
@@ -0,0 +1,24 @@
+
+/* 
+ * GMTK_Constants.h
+ * Contains widely used type definintions and constants.
+ *
+ * Written by Geoffrey Zweig <gzweig@us.ibm.com>
+ * 
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle make no representations about the suitability of this software 
+ * for any purpose. It is provided "as is" without express or implied warranty.
+ *
+ */ 
+
+#ifndef GMTK_CONSTANTS_H
+#define GMTK_CONSTANTS_H
+
+typedef DISCRETE_VARIABLE_TYPE unsigned short;
+
+#endif
