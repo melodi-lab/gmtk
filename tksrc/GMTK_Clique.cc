674f6db3b1ab1c70cb0c6eee14ecfe49356c88b8
zweig <zweig@localhost>
2001-04-25 01:17:11 +0000

    wrote enumerativeValues

--- tksrc/GMTK_Clique.cc
+++ tksrc/GMTK_Clique.cc
@@ -16,4 +16,63 @@
  *
  */ 
 
+void GMTK_Clique::cacheClampedValues()
+{
+    for (int i=0; i<discreteMember.len(); i++)
+        clampedValues[i] = discreteMember[i]->val;
+}
+
+logpr GMTK_Clique::probGivenParents()
+{
+    logpr p = 1;
+    findConditionalProbabilityNodes();
+    for (int i=0; i<conditionalProbabilityNode.len(); i++)
+        p *= conditionalProbabilityNode[i]->probGivenParents();
+    return p;
+}
+
+void GMTK_Clique::enumerateValues(int new_member_num, cliqueValue *pred_val,
+bool viterbi)
+{
+    if (separator)
+    {
+        cacheClampedValues();
+     
+        // make sure something is in the table.
+        if (instantiation.find(campedValues) == instantiation.end())
+        {
+            cliqueValue cv;
+            cv.pi = 0;
+            instantiation[clampedValues] = cv;
+        }
+        
+        if (!viterbi)
+        {
+            instantiation[clampedValues].pi += pred_val->pi;
+            pred_val->succ = &instantiation[clampedValues];
+        }
+        else if (pred_val->pi >= instantiation[clampedValues].pi)
+        {
+            instantiation[clampedValues].pi = pred_val->pi;
+            instantiation[clampedValues].pred = pred_val;
+        }
+    }
+    else if (new_member_num == newMember.len())  // base case: all members fixed
+    {
+        cacheClampedValues();
+        cliqueValue cv;
+        cv.pi = cv.lambda = probGivenParents();  // cache value in lambda
+        cv.pred = pred_val;
+        if (pred_val)   // not doing root
+            cv.pi *= pred_val->pi;
+        instantiation[clampedValues] = cv;
+    }
+    else
+    {
+        newMember[new_member_num]->clampFirstValue();
+        do
+        {
+            enumerateValues(new_member_num+1, pred_val, viterbi);
+        } while (member[new_member_num]->clampNextValue();
+}
 
