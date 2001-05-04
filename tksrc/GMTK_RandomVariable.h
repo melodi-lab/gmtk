9987726f683b7316cddb780e25d5437faefa6cf8
Jeff Bilmes <bilmes@users.noreply.github.com>
2001-05-04 20:36:38 +0000

    *** empty log message ***

--- tksrc/GMTK_RandomVariable.h
+++ tksrc/GMTK_RandomVariable.h
@@ -3,7 +3,7 @@
  * Defines the functions that all random variables must satisfy in order
  * for the inference, sampling, and other generic routines to work.
  *
- * Written by Geoffrey Zweig <gzweig@us.ibm.com>
+ * Written by Jeff Bilmes <bilmes@ee.washington.edu> & Geoffrey Zweig <gzweig@us.ibm.com>
  * 
  * Copyright (c) 2001, < fill in later >
  *
@@ -22,12 +22,8 @@
 #include "sArray.h"
 #include "logp.h"
 
-/* The random variable class defines the basic functions that random
-   variables must implement. 
+#include "GMTK_RngDecisionTree.h"
 
-   Switching parents are guaranteed to be discrete.
-   A nodes conditioning parents must be lower indexed than it.
-*/
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -35,26 +31,42 @@
 // may take on. Possibilities include unsigned char, char, short, int, 
 // unsigned long, and so on.
 #define DISCRETE_VARIABLE_TYPE Int16
+//
 
 
+/*
+ * The random variable class defines the basic functions that random
+ * variables must implement. 
+ *
+ * Switching parents are guaranteed to be discrete.
+ * A nodes conditioning parents must be lower indexed than it.
+ *
+ */
 class RandomVariable
 {
 public:
 
-    int nodeNum;  
-    // A unique node index. nodes in a GM are numbered consecutively from 0.
+    RandomVariable();
+    virtual ~RandomVariable() {}
 
-    int timeIndex;
+    /////////////////////////////////////////////////////////////////////////
+    // A unique node index. nodes in a GM are numbered consecutively from 0.
+    int nodeNum;  
+
+    /////////////////////////////////////////////////////////////////////////
     // What time frame does the node belong to?
     // Counting starts from 0.
+    int timeIndex;
 
-    bool hidden;
+    /////////////////////////////////////////////////////////////////////////
     // Is the node hidden, or is it an observation.
+    bool hidden;
 
-    bool discrete;
+    /////////////////////////////////////////////////////////////////////////
     // Is the variable discrete?
     // Inference conditions on this; cliques keep track of the values of 
     // their discrete members.
+    bool discrete;
 
     //////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////
@@ -62,35 +74,37 @@ public:
     //////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////
 
-// Q: Ideally this stuff should be moved to the child class.
-// After that is finished, see if we can move it there.
+    // Q: Ideally this stuff should be moved to the child class.
+    // After that is finished, see if we can move it there.
 
-    DISCRETE_VARIABLE_TYPE val;
+    /////////////////////////////////////////////////////////////////
     // in the discrete case, the actual value of the variable.
     // Cliques keep track of the values of their discrete members.
-
-    const int cardinality;
+    DISCRETE_VARIABLE_TYPE val;
+  
+    ///////////////////////////////////////////////////////
     // Again in the discrete case.
     // The maximum number of possible values this RV may take in, irrespective
     // of the values of the parents. So, we must
     // have that 0 <= val < cardinality
-    sArray<RandomVariable *> switchingParents, *curConditionalParents;
+    const int cardinality;
 
-    // The probability of a variable taking a particular value depends on
-    // the values of its parents. There come in two varieties: switching 
-    // parents and conditional parents. The values of the switching parents
-    // determines an additional set of conditional parents. The variable
-    // is conditioned on these conditional parents. Note that a switching 
-    // parent may also be a conditional parent.
-    // A node's switching and conditional parents must all be lower numbered 
-    // than the node itself, thus allowing a topological ordering of 
+    ////////////////////////////////////////////////////////////////////////
+    // The set of switching parents, if any.
+    sArray<RandomVariable *> switchingParents;
 
-    sArray<RandomVariable *> allPossibleParents;
+    ////////////////////////////////////////////////////////////////////////
     // allPossibleParents is the union of the switchingParents and the
     // all possible conditionalParents.
     // Used to determine topological orderings
+    sArray<RandomVariable *> allPossibleParents;
 
-    sArray< sArray < RandomVariable* > > conditionalParentsList;
+    ////////////////////////////////////////////////////////////////////////
+    // The set of variables that use this variable either as a switching
+    // parent or as a (possible) conditional parent.
+    sArray<RandomVariable *> allPossibleChildren;
+
+    ////////////////////////////////////////////////////////////////////////
     // For each possible different list of
     // conditional parents that might exist for all possible
     // values of the switching parents, this array gives that list of
@@ -101,34 +115,60 @@ public:
     // parents, and those are the only two set of conditional parents
     // that exist for all values of the switching parents, this
     // list is of size two. 
+    sArray< sArray < RandomVariable* > > conditionalParentsList;
 
+
+    ////////////////////////////////////////////////////////////////////////
+    // This is set to the current set of conditional parents,
+    // which is dependent on the current value of the switching parents.
+    // Note that this points to one of the entries in conditionalParentsList
+    sArray<RandomVariable *> *curConditionalParents;
+
+    ////////////////////////////////////////////////////////////////////////
+    // This decision tree is used by the intFromSwitchingState() routine
+    // to map from the current set of switching parent values
+    // to an integer, which indicates which set of conditional
+    // parents should be active for those switching parent values.
+    RngDecisionTree<int> *dtMapper;
+
+    ////////////////////////////////////////////////////////////////////////
+    // A method to make it easy to set up the graph topology
+    // The probability of a variable taking a particular value depends on
+    // the values of its parents. There come in two varieties: switching 
+    // parents and conditional parents. The values of the switching parents
+    // determines an additional set of conditional parents. The variable
+    // is conditioned on these conditional parents. Note that a switching 
+    // parent may also be a conditional parent.
+    // A node's switching and conditional parents must all be lower numbered 
+    // than the node itself, thus allowing a topological ordering of 
     void setParents(sArray<RandomVariable *> &sparents, 
          sArray<sArray<RandomVariable *> > &cpl);
-    // A method to make it easy to set up the graph topology
-    
 
-    sArray<RandomVariable *> allPossibleChildren;
-    // The set of variables that use this variable either as a switching
-    // parent or as a (possible) conditional parent.
-
-    int intFromSwitchingState();
+    ////////////////////////////////////////////////////////////////////////
     // Looks at the currently (presumably clamped) values of the
     // switching parents and returns an 1D integer index. 
     // This indexes the set of conditional parents to use.
+    int intFromSwitchingState() 
+    {
+      return dtMapper->query(switchingParents);
+    }
 
-    virutal void findConditionalParents() = 0;
+    ////////////////////////////////////////////////////////////////////////
     // Looks at the values of the switching parents, and sets the 
     // conditionalParents array pointer appropriately.
+    virtual void findConditionalParents() = 0;
 
-    void findAllPossibleParents();
+    ////////////////////////////////////////////////////////////////////////
     // Iterates through all possible instantiations of the conditional
     // parents and unions together the parents. Stores the result in the
-    // allPossibleParents array (in what order??)
+    // allPossibleParents array (in what order?)
+    void findAllPossibleParents();
 
-    int address(sArray<RandomVariable *>);
+    ////////////////////////////////////////////////////////////////////////
     // Looks up the values of a vector of parents and computes an
     // integer index into a 1 dimensional array. 
-    //  (not sure what this is for??)
+    //  (not sure what this is for?)
+    int address(sArray<RandomVariable *>);
 
     //////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////
@@ -136,52 +176,64 @@ public:
     //////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////
 
-    virtual logpr probGivenParents() = 0;
+    ////////////////////////////////////////////////////////////////////////
     // The inference algorithm guarantees that when this is called, the
     // variable and its parents will be clamped to appropriate values.
     // This also assumes that findConditionalParents has been called.
+    virtual logpr probGivenParents() = 0;
 
-    virtual void clampFirstValue() = 0;
+    ////////////////////////////////////////////////////////////////////////
     // Sets a variable to the first value that is possible.
     // Values with 0 probability can be ignored, as long as some value is set.
     // This function must always do a clamping.
     // Observation variables (already clamped do nothing).
     // It is assumed that the parent values are clamped at this point.
+    virtual void clampFirstValue() = 0;
 
-    virtual bool clampNextValue() = 0;
+    ////////////////////////////////////////////////////////////////////////
     // Sets a variable to the next possible value.
     // Returns false when there are no more values or the variable is an
     // observation.
     // It is assumed that the parent values are clamped at this point.
+    virtual bool clampNextValue() = 0;
 
-    virtual void makeRandom() = 0;
+    ////////////////////////////////////////////////////////////////////////
     // Sets the parameters determining probGivenParents() to random values.
+    virtual void makeRandom() = 0;
 
-    virtual void makeUniform() = 0;
+    ////////////////////////////////////////////////////////////////////////
     // Sets the parameters determining probGivenParents() to uniform values.
+    virtual void makeUniform() = 0;
 
-    virtual void instantiate() = 0;
+    ////////////////////////////////////////////////////////////////////////
     // Sets the variable according to the probability distribution determined
     // by its parent's values. (i.e., sample from this distribution)
     // Used for simulation.
+    virtual void instantiate() = 0;
 
-    virtual void tieWith(RandomVariable *rv) = 0;
+    ////////////////////////////////////////////////////////////////////////
     // In a DGM, the analogous occurrences of a variable in different time
     // slices all share the same distributions and accumulators. This function
     // tells a variable to use the data structures associated with just one
     // member of its equivalence class.
+    virtual void tieWith(RandomVariable *rv) = 0;
 
-    virtual void cacheValue() = 0;
+
+    ////////////////////////////////////////////////////////////////////////
     // It can be useful to tell a variable to cache the value it's currently
     // set to. For example, wheen keeping track of the best values seen
     // so far in enumerativeViterbi(). Calling this function tells a variable
     // to store its current value (wherever it wants).
     // Not expected to be called many times, so just leave it virtual.
     // This like a push operation, onto a one element size stack.
+    virtual void cacheValue() = 0;
 
-    virtual void restoreCachedValue() = 0;
+
+    ////////////////////////////////////////////////////////////////////////
     // Sets the variable's value to its cached value.
     // This like a pop operation, off of the stack.
+    virtual void restoreCachedValue() = 0;
+
 
     /////////////////////////////////////////
     /////////////////////////////////////////
@@ -189,10 +241,12 @@ public:
     /////////////////////////////////////////
     /////////////////////////////////////////
 
-    virtual void zeroAccumulators() = 0;
+    ////////////////////////////////////////////////////////////////////////
     // Called at the beginning of an EM iteration.
+    virtual void zeroAccumulators() = 0;
 
-    virtual void increment(logpr posterior) = 0;
+
+    ////////////////////////////////////////////////////////////////////////
     // On the backward pass, this will be called with a posterior probability
     // for the variable and its parents with all possible sets of values.
     // In inference, the GM is used as a global memory, with variable values
@@ -203,10 +257,14 @@ public:
     // Then the count of seeing the variable and its parents with those values
     // is incremented by the posterior amount.
     // For continuous variables, statistics are accumulated.
+    virtual void increment(logpr posterior) = 0;
 
-    virtual void update() = 0;
+
+    ////////////////////////////////////////////////////////////////////////
     // At the end of each EM iteration, this is called to convert the 
     // accumulated statistics into probability distributions.
+    virtual void update() = 0;
+
 
     /////////////////////////////////////////
     /////////////////////////////////////////
