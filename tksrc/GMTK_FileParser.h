dc0d9d3d534b3ab011fdd168031f8836aa9b4e05
Jeff Bilmes <bilmes@users.noreply.github.com>
2001-05-08 08:39:30 +0000

    *** empty log message ***

--- tksrc/GMTK_FileParser.h
+++ tksrc/GMTK_FileParser.h
@@ -19,9 +19,13 @@
 #ifndef GMTK_FILEPARSER_H
 #define GMTK_FILEPARSER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+//  #include "sArray.h"
+//  #include "logp.h"
+
 /*
-#include "sArray.h"
-#include "logp.h"
 #include "GMTK_GM.h" 
 */
 
@@ -31,34 +35,95 @@ class FileParser
 
   //  GMTK_GM& gm; 
 
+  //////////////////////////////////
+  // The file we are currently parsing.
+  FILE *ifile;
+
 public:
 
-  enum {
-    TT_INT=0,
-    TT_FLT=1,
-    TT_KWD=2,
-    TT_ID=3,
-    TT_OP=4,
-    TT_UNDEF=5
+  enum TokenType {
+    Token_Integer=0,
+    Token_Real=1,
+    Token_Colon=2,
+    Token_SemiColon=3,
+    Token_LeftBrace=4,
+    Token_RightBrace=5,
+    Token_LeftParen=6,
+    Token_RightParen=7,
+    Token_VirtBar=8,
+    Token_Keyword=9,
+    Token_Identifier=10,
+    Token_Undefined=11
   };
 
+  struct TokenInfo {
+    // the line in the file of the most recently processed token
+    int srcLine; 
+    // the char on the current line of most rec. proc. token
+    int srcChar; 
 
+    // the token in string form
+    char *tokenStr; 
+
+    // the type of the token
+    TokenType tokenType;
+
+    ///////////////////////////////////////////
+    // if of the appropriate type, the value
+    // of the read token.
+    union {
+      int int_val;
+      float float_val;
+      double doub_val;
+    };
+
+    ///////////////////////////////////////////////////////////////
+    // a simple string equality check
+    bool operator == (const char *const s) { return !::strcmp(s,tokenStr); }
+    bool operator != (const char *const s) { return ::strcmp(s,tokenStr); }
+
+    ///////////////////////////////////////////////////////////////
+    // a simple token type equality check
+    bool operator == (const enum TokenType t) { return t == tokenType; }
+    bool operator != (const enum TokenType t) { return t != tokenType; }
+
+  };
+
+  /////////////////////////////////////
+  // filled in automatically by the scanner.
+  static TokenInfo tokenInfo;
+
+  void setTokenPosition();
+
+
+  //////////////////////////////////////////////
   //  FileParser(const char *const fileName,
   //	     GMTK_GM& gm);
 
-
+  //////////////////////////////////////////////
+  // constructor opens and parses file, or dies if an
+  // error occurs.
   FileParser(const char *const fileName);
 
-  parseGraphicalModel();
-  parseFrameList();
-  parseFrame();
-  parseRandomVariableList();
-  parseRandomVariable();
-  parseRandomVariableBody();
-
-
-	     
-
+  void parseGraphicalModel();
+  void parseFrameList() {}
+  void parseFrame() {}
+  void parseRandomVariableList();
+  void parseRandomVariable();
+  void parseRandomVariableAttributeList();
+  void parseRandomVariableAttribute();
+  void parseRandomVariableType();
+  void parseRandomVariableDisposition();
+  void parseSwitchingParentList();
+  void parseParentList();
+  void parseMappingSpec();
+  void parseConditionalParentListList();
+  void parseConditionalParentList();
+  void parseCPT_SPEC();
+  void parseCPT_TYPE();
+  void parseParent();
+  void parseContinousImplementation();
+  void parseChunkSpecifier() {}
 
 };
 
