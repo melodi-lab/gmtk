3173b447002bf23fc7f39d625ac53eb4096aefec
bilmes <bilmes@localhost>
2001-05-13 07:09:36 +0000

    Initial revision

---
+++ tksrc/GMTK_MTCPT.cc
@@ -0,0 +1,313 @@
+/*-
+ * GMTK_MTCPT.cc
+ *     A Multi-Dimensional dense Conditional Probability Table class.
+ *
+ * Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ *
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle, and Jeff Bilmes make no representations about
+ * the suitability of this software for any purpose.  It is provided
+ * "as is" without express or implied warranty.
+ *
+ */
+
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <float.h>
+#include <assert.h>
+
+#include "general.h"
+#include "error.h"
+#include "rand.h"
+
+#include "GMTK_MTCPT.h"
+#include "GMTK_GMParms.h"
+
+VCID("$Header$");
+
+
+
+
+////////////////////////////////////////////////////////////////////
+//        General create, read, destroy routines 
+////////////////////////////////////////////////////////////////////
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * MTCPT::MTCPT()
+ *      Constructor
+ *
+ * Results:
+ *      Constructs the object.
+ *
+ * Side Effects:
+ *      None so far.
+ *
+ *-----------------------------------------------------------------------
+ */
+MTCPT::MTCPT()
+{
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * MTCPT::setNumParents()
+ *      Just sets the number of parents and resizes arrays as appropriate.
+ *
+ * Results:
+ *      no results.
+ *
+ * Side Effects:
+ *      Will change internal arrays of this object.
+ *
+ *-----------------------------------------------------------------------
+ */
+void MTCPT::setNumParents(const int _nParents)
+{
+  CPT::setNumParents(_nParents);
+  bitmask &= ~bm_basicAllocated;
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * MTCPT::setNumCardinality(var,card)
+ *      sets the cardinality of var to card
+ *
+ * Results:
+ *      no results.
+ *
+ * Side Effects:
+ *      Will change internal array content of this object.
+ *
+ *-----------------------------------------------------------------------
+ */
+void MTCPT::setNumCardinality(const int var, const int card)
+{
+
+  CPT::setNumCardinality(var,card);
+  // assume that the basic stuff is not allocated.
+  bitmask &= ~bm_basicAllocated;
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * MTCPT::allocateBasicInternalStructures()
+ *      Allocates remainder of internal data structures assuming
+ *      that numParents and cardinalities are called.
+ *
+ * Results:
+ *      no results.
+ *
+ * Side Effects:
+ *      Will change internal content of this object.
+ *
+ *-----------------------------------------------------------------------
+ */
+void MTCPT::allocateBasicInternalStructures()
+{
+  error("MTCPT::allocateBasicInternalStructures() not implemented");
+  // basic stuff is now allocated.
+  bitmask |= bm_basicAllocated;
+}
+
+
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * MTCPT::read(is)
+ *      read in the table.
+ * 
+ * Results:
+ *      No results.
+ *
+ * Side Effects:
+ *      Changes the 'mscpt' member function in the object.
+ *
+ *-----------------------------------------------------------------------
+ */
+
+void
+MTCPT::read(iDataStreamFile& is)
+{
+
+  NamedObject::read(is);
+  is.read(numParents,"MTCPT::read numParents");
+
+  if (numParents < 0) 
+    error("MTCPT: read, trying to use negative (%d) num parents.",numParents);
+  if (numParents >= warningNumParents)
+    warning("MTCPT: read, creating MTCPT with %d parents",numParents);
+  cardinalities.resize(numParents+1);
+  // read the cardinalities
+  for (unsigned i=0;i<=numParents;i++) {
+    is.read(cardinalities[i],"MTCPT::read cardinality");
+    if (cardinalities[i] <= 0)
+      error("MTCPT: read, trying to use 0 or negative (%d) cardinality table.",cardinalities[i]);
+  }
+
+  // Finally read in the integer ID of the decision tree
+  // that maps from parent values to an integer specifying
+  // the sparse CPT. 
+  is.read(dtIndex);
+  if (dtIndex < 0 || dtIndex >= GM_Parms.dts.size())
+    error("MTCPT::read, invalid DT index %d\n",dtIndex);
+
+  // TODO: check that the cardinalities of self match
+  // with that of the dt.
+
+  //////////////////////////////////////////////////////////
+  // set the DT
+  dt = GM_Parms.dts[dtIndex];
+
+  bitmask |= bm_basicAllocated;
+}
+
+
+/*-
+ *-----------------------------------------------------------------------
+ * MTCPT::write(os)
+ *      write out data to file 'os'. 
+ * 
+ * Results:
+ *      No results.
+ *
+ * Side Effects:
+ *      No effects other than  moving the file pointer of os.
+ *
+ *-----------------------------------------------------------------------
+ */
+void
+MTCPT::write(oDataStreamFile& os)
+{
+  NamedObject::write(os);
+  os.write(numParents,"MTCPT::write numParents");
+  os.writeComment("number parents");os.nl();
+  for (unsigned i=0;i<=numParents;i++) {
+    os.write(cardinalities[i],"MTCPT::write cardinality");
+  }
+  os.writeComment("cardinalities");
+  os.nl();
+  os.write(dtIndex);
+  os.nl();
+}
+
+
+////////////////////////////////////////////////////////////////////
+//        Test Driver
+////////////////////////////////////////////////////////////////////
+
+#ifdef MAIN
+
+#include "fileParser.h"
+
+GMParms GM_Parms;
+
+int
+main()
+{
+
+  MTCPT mtcpt;
+
+  // this is a binary variable with three parents
+  // the first one is binary, the second one is ternary,
+  // and the third one is binary.
+
+  mtcpt.setNumParents(3);
+  mtcpt.setNumCardinality(0,2);
+  mtcpt.setNumCardinality(1,3);
+  mtcpt.setNumCardinality(2,2);
+  mtcpt.setNumCardinality(3,3);
+  mtcpt.allocateBasicInternalStructures();
+
+  // set to random values
+  mtcpt.makeRandom();
+  
+  // write values to a data file in ASCII.
+  oDataStreamFile od ("/tmp/foo.mtcpt",false);
+
+  mtcpt.write(od);
+
+  // now print out some probabilities.
+  vector < int > parentVals;
+  vector < int > cards;
+
+  parentVals.resize(3);
+  cards.resize(3);
+  
+  cards[0] = cards[1] = cards[2] = 5;
+
+  parentVals[0] = 0;
+  parentVals[1] = 0;
+  parentVals[2] = 0;
+
+  for (int i =0; i<3;i++) {
+    printf("Prob(%d) Given cur Par = %f\n",
+	   i,mtcpt.probGivenParents(parentVals,cards,i).unlog());
+  }
+
+  parentVals[0] = 0;
+  parentVals[1] = 0;
+  parentVals[2] = 1;
+
+  for (int i =0; i<3;i++) {
+    printf("Prob(%d) Given cur Par = %f\n",
+	   i,mtcpt.probGivenParents(parentVals,cards,i).unlog());
+  }
+
+  parentVals[0] = 0;
+  parentVals[1] = 1;
+  parentVals[2] = 1;
+
+  for (int i =0; i<3;i++) {
+    printf("Prob(%d) Given cur Par = %f\n",
+	   i,mtcpt.probGivenParents(parentVals,cards,i).unlog());
+  }
+
+  parentVals[0] = 1;
+  parentVals[1] = 2;
+  parentVals[2] = 1;
+
+  for (int i =0; i<3;i++) {
+    printf("Prob(%d) Given cur Par = %f\n",
+	   i,mtcpt.probGivenParents(parentVals,cards,i).unlog());
+  }
+
+  // Now iterate over valid values.
+  MTCPT::iterator it = mtcpt.begin();
+  do {
+    printf("Prob of %d is %f\n",
+	   it.val(),it.probVal.unlog());
+  } while (it != mtcpt.end());
+
+
+  parentVals[0] = 0;
+  parentVals[1] = 0;
+  parentVals[2] = 1;
+  mtcpt.becomeAwareOfParentValues(parentVals,cards);
+
+  it = mtcpt.begin();
+  do {
+    printf("Prob of %d is %f\n",
+	   it.val(),it.probVal.unlog());
+  } while (it != mtcpt.end());
+
+
+}
+
+
+#endif
---
+++ tksrc/GMTK_MTCPT.h
@@ -0,0 +1,167 @@
+/*-
+ * GMTK_MTCPT.h
+ *
+ *  Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ * 
+ *  $Header$
+ * 
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle make no representations about
+ * the suitability of this software for any purpose.  It is provided
+ * "as is" without express or implied warranty.
+ *
+ */
+
+
+#ifndef GMTK_MTCPT_H
+#define GMTK_MTCPT_H
+
+#include <vector>
+
+#include "fileParser.h"
+#include "logp.h"
+
+#include "GMTK_RngDecisionTree.h"
+#include "GMTK_RandomVariable.h"
+#include "GMTK_CPT.h"
+#include "GMTK_Sparse1DPMF.h"
+
+#include "GMTK_EMable.h"
+#include "GMTK_GMParms.h"
+#include "GMTK_NamedObject.h"
+
+
+class MTCPT : public EMable, public CPT, public NamedObject {
+
+  //////////////////////////////////
+  // Index into the world structure
+  // of the decision tree
+  unsigned dtIndex; 
+
+  ///////////////////////////////////////
+  // Direct pointer to the decision tree.
+  RngDecisionTree<int>* dt;
+
+  ////////////////
+  // the value
+  int _val;
+
+public:
+
+  ///////////////////////////////////////////////////////////  
+  // General constructor
+  MTCPT();
+  ~MTCPT() { }
+
+  ///////////////////////////////////////////////////////////    
+  void setNumParents(const int _nParents);
+  void setNumCardinality(const int var, const int card);
+  void allocateBasicInternalStructures();
+  ///////////////////////////////////////////////////////////    
+
+  //////////////////////////////////
+  // various forms of probability calculation
+  void becomeAwareOfParentValues( vector <int>& parentValues,
+				  vector <int>& cards ) {
+    _val = dt->query(parentValues,cards);
+  }
+  void becomeAwareOfParentValues( vector <RandomVariable *>& parents ) {
+    _val = dt->query(parents);
+  }
+  logpr probGivenParents(const int val) {
+    assert ( bitmask & bm_basicAllocated );
+    assert ( val >= 0 && val <= cardinalities[numParents] );
+    if (val == _val)
+      return 1.0;
+    else
+      return 0.0;
+  }
+  logpr probGivenParents(vector <int>& parentValues, 
+			 vector <int>& cards,
+			 const int val) {
+    assert ( bitmask & bm_basicAllocated );
+    becomeAwareOfParentValues(parentValues,cards);
+    return probGivenParents(val);
+  }
+  logpr probGivenParents(vector <RandomVariable *>& parents,
+			 const int val) {
+    assert ( bitmask & bm_basicAllocated );
+    becomeAwareOfParentValues(parents);
+    return probGivenParents(val);
+  }
+  int numValsGivenParents() { 
+    assert ( bitmask & bm_basicAllocated );
+    return cardinalities[numParents]; 
+  }
+
+  // returns an iterator for the first one.
+  iterator begin() {
+    assert ( bitmask & bm_basicAllocated );
+    iterator it;
+    it.internalState = 0;
+    it.probVal = 1.0;
+    return it;
+  }
+
+  iterator end() {
+    assert ( bitmask & bm_basicAllocated );
+    iterator it(this);
+    it.internalState = 1;
+    return it;
+  }
+  bool next(iterator &it) {
+    assert ( bitmask & bm_basicAllocated );
+    // don't increment past the last value.
+    if (it.internalState == 1)
+      return false;
+    it.internalState = 1;
+    it.probVal = 0.0;
+    return true;
+  }
+  virtual int valueAtIt(const int internalState) { 
+    assert ( internalState == 0);
+    return _val;
+  }
+
+  // random sample given current parents value
+  int randomSample() { return _val; }
+  
+  //////////
+  // these routines do nothing for an MTCPT since
+  // there is only one possible value for a DT given the parents.
+  ///////////////////////////////////////////////////////////  
+  // Re-normalize the output distributions
+  void normalize() {}
+  // set all values to random values.
+  void makeRandom() {}
+  // set all values to uniform values.
+  void makeUniform() {}
+
+  //////////////////////////////////////////////
+  // read/write basic parameters
+  void read(iDataStreamFile& is);
+  void write(oDataStreamFile& os);
+
+  //////////////////////////////////
+  // Public interface support for EM
+  //////////////////////////////////
+  void emInit() {}
+  void emStartIteration() {}
+  void emIncrement(logpr prob) {}
+  void emEndIteration() {}
+  void emSwapCurAndNew() {}
+  void emStoreAccumulators(oDataStreamFile& ofile) {}
+  void emLoadAccumulators(iDataStreamFile& ifile) {}
+  void emAccumulateAccumulators(iDataStreamFile& ifile) {}
+  //////////////////////////////////
+
+};
+
+
+
+#endif // defined MTCPT
