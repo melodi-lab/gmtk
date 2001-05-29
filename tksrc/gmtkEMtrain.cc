50235f0775995287aaff125be69bb71cd17f53fb
bilmes <bilmes@localhost>
2001-05-29 19:11:18 +0000

    *** empty log message ***

--- tksrc/gmtkEMtrain.cc
+++ tksrc/gmtkEMtrain.cc
@@ -2,7 +2,9 @@
  * gmtkEMtrain.cc
  *     Train up a GM using EM
  *
- * Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ * Written by:
+ *   Jeff Bilmes <bilmes@ee.washington.edu>
+ *   Geoffrey Zweig <gzweig@us.ibm.com>
  *
  * Copyright (c) 2001, < fill in later >
  *
@@ -79,12 +81,6 @@ main(int argc,char*argv[])
   if (seedme)
     rnd.seed();
 
-  logpr beam=pruneRatio;
-
-  /////
-  // open the observation file
-  globalObservationMatrix.openFile(obsFileName);
-
   /////////
   // read in all parameters
   iDataStreamFile parmsFile(parmsFileName);
@@ -107,9 +103,12 @@ main(int argc,char*argv[])
   // link the RVs with the parameters that are contained in
   // the bn1_gm.dt file.
   fp.associateWithDataParams();
+
   // now associate the RVs with a GM
   GMTK_GM gm;
   fp.addVariablesToGM(gm);
+
+  gm.setExampleStream(obsFileName);
 
   gm.verifyTopologicalOrder();
 
@@ -119,53 +118,7 @@ main(int argc,char*argv[])
   // printf("randomizing parameters\n");
   // gm.makeRandom();
 
-  logpr previous_likelihood = 0.0;
-  for (unsigned emIter=0;emIter<maxEmIterations;emIter++) {
-    gm.emStartIteration();
-    logpr likelihood = 0.0;
-    for (unsigned segment=0;
-	 segment<globalObservationMatrix.numSegments();
-	 segment++) {
-      globalObservationMatrix.loadSegment(segment);
-
-      //////////////////////////////
-      // TODO: right now, need to 're-unroll' the network in each case,
-      // This is terribly inefficient and it needs to be fixed.
-      
-      if (gm.chain)
-	delete gm.chain;
-
-      if (((globalObservationMatrix.numFrames 
-	    - fp.firstChunkframe()
-	    - (fp.maxFrame() - fp.lastChunkframe()))
-	   % 
-	   (fp.lastChunkframe()-fp.firstChunkframe()+1)) != 0) {
-	error("ERROR: segment %d, number of frames %d minus %d must be a multiple of %d\n",segment,
-	      globalObservationMatrix.numFrames,
-	      fp.firstChunkframe()+(fp.maxFrame()-fp.lastChunkframe()),
-	      (fp.lastChunkframe()-fp.firstChunkframe()+1));
-      }
-
-      gm.unroll(fp.firstChunkframe(), fp.lastChunkframe(), 
-		(globalObservationMatrix.numFrames 
-		 - fp.firstChunkframe()
-		 - (fp.maxFrame() - fp.lastChunkframe()))
-		/
-		(fp.lastChunkframe()-fp.firstChunkframe()+1)
-		);
-      if (!gm.chain->computePosteriors(beam)) {
-	printf("Skipping segment %d due to 0 probability\n",segment);
-	continue;
-      }
-      likelihood *= gm.chain->dataProb;
-    }
-    printf("Total data likelihood is %e\n",likelihood.val());
-    gm.emEndIteration();
-    if (likelihood > previous_likelihood) {
-      gm.emSwapCurAndNew();
-    }
-    previous_likelihood = likelihood;
-  }
+  gm.cliqueChainEM(maxEMIterations, pruneRatio);
 
   return 0;  
 
