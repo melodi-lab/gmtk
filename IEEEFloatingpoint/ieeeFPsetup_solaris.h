86e332951ebf92dd6a08951e4a95b48ee65e9eb6
Jeff Bilmes <bilmes@users.noreply.github.com>
2001-06-02 01:55:38 +0000

    Initial revision

---
+++ IEEEFloatingpoint/ieeeFPsetup_solaris.h
@@ -0,0 +1,16 @@
+//
+//
+// $Header$
+// Written by: Jeff Bilmes
+//             bilmes@icsi.berkeley.edu
+
+
+#ifndef IEEE_FP_SETUP_H
+#define IEEE_FP_SETUP_H
+
+#include <ieeefp.h>
+#include <float.h>
+
+void ieeeFPsetup();
+
+#endif
