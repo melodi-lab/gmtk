3b33de68da3c8781ba9256fc0338e8789f9ca87d
bilmes <bilmes@localhost>
2001-06-02 03:05:20 +0000

    Initial revision

---
+++ tksrc/GMTK_EMable.cc
@@ -0,0 +1,50 @@
+/*-
+ * GMTK_EMable.cc
+ *     A vector used for means of Gaussians.
+ *
+ * Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ *
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle, and Jeff Bilmes make no representations about
+ * the suitability of this software for any purpose.  It is provided
+ * "as is" without express or implied warranty.
+ *
+ */
+
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <ieeefp.h>
+#include <float.h>
+#include <assert.h>
+
+#include "general.h"
+#include "error.h"
+#include "rand.h"
+#include "logp.h"
+
+#include "GMTK_EMable.h"
+#include "GMTK_GMParms.h"
+
+VCID("$Header$");
+
+logpr
+EMable::minIncrementProbabilty = 0.0;
+
+unsigned long
+EMable::missedIncrementCount = 0;
+
+
+////////////////////////////////////////////////////////////////////
+//        General create, read, destroy routines 
+////////////////////////////////////////////////////////////////////
+
