869a1f35a5324b05063e461b35a7547ab3dec006
zweig <zweig@localhost>
2001-06-30 19:08:01 +0000

    alignment not printed when all paths have 0 prob (pruning)

--- tksrc/gmtkViterbi.cc
+++ tksrc/gmtkViterbi.cc
@@ -1,24 +1,35 @@
+
+/*
+ * "Copyright 2001, International Business Machines Corporation and University
+ * of Washington. All Rights Reserved
+ *
+ *    Written by Geoffrey Zweig and Jeff Bilmes
+ *
+ * NO WARRANTY
+ * THE PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT
+ * LIMITATION, ANY WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Each Recipient is
+ * solely responsible for determining the appropriateness of using the Program
+ * and assumes all risks associated with such use, including but not limited
+ * to the risks and costs of program errors, compliance with applicable laws,
+ * damage to or loss of data, programs or equipment, and unavailability or
+ * interruption of operations.
+
+ * DISCLAIMER OF LIABILITY
+ * THE UNIVERSITY OF WASHINGTON, INTERNATIONAL BUSINESS MACHINES CORPORATION,
+ * GEOFFREY ZWEIG AND JEFF BILMES SHALL NOT HAVE ANY LIABILITY FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING WITHOUT LIMITATION LOST PROFITS), HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  OF
+ * THE PROGRAM, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES."
+*/
+
 /*-
  * gmtkViterbi.cc
  *     Get the viterbi instantiation for each example in a data set 
- *
- * Written by:
- *   Geoffrey Zweig <gzweig@us.ibm.com>
- *   Jeff Bilmes <bilmes@ee.washington.edu>
- *
- * Copyright (c) 2001, < fill in later >
- *
- * Permission to use, copy, modify, and distribute this
- * software and its documentation for any non-commercial purpose
- * and without fee is hereby granted, provided that the above copyright
- * notice appears in all copies.  The University of Washington,
- * Seattle, and Jeff Bilmes make no representations about
- * the suitability of this software for any purpose.  It is provided
- * "as is" without express or implied warranty.
- *
  */
-
-
 
 #include <math.h>
 #include <stdlib.h>
@@ -169,7 +180,7 @@ main(int argc,char*argv[])
          << ((*gm.node.rbegin())->timeIndex+1) << " frames\n";
     if (showVitVals)
       gm.reveal(gm.node, true);
-    if (wordVar)
+    if (wordVar && gm.viterbiProb!=0.0)
     {
       // print the sequence of values for this variable
       // compress consecutive values into a single instance
