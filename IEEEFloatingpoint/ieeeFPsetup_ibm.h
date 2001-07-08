4a9ebd04fc60774137f6322309a3b56b6cb384f8
Jeff Bilmes <bilmes@users.noreply.github.com>
2001-07-08 22:12:45 +0000

    Initial revision

---
+++ IEEEFloatingpoint/ieeeFPsetup_ibm.h
@@ -0,0 +1,15 @@
+//
+//
+// $Header$
+// Written by: Jeff Bilmes
+//             bilmes@icsi.berkeley.edu
+
+
+#ifndef IEEE_FP_SETUP_H
+#define IEEE_FP_SETUP_H
+
+#include <float.h>
+
+void ieeeFPsetup();
+
+#endif
