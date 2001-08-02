7243d65bb8d3322046e36511a49705f68687804b
Jeff Bilmes <bilmes@users.noreply.github.com>
2001-08-02 18:49:06 +0000

    added double precision versions.

--- miscSupport/lineqsolve.h
+++ miscSupport/lineqsolve.h
@@ -23,3 +23,20 @@ extern void
 lineqsolve(const int n, const int nrhs,
 	   float *a,
 	   float *b);
+
+extern void 
+lubksb(double *a, 
+       int n, 
+       int *indx, 
+       double *b);
+
+extern void 
+ludcmp(double *a,
+       int n, 
+       int *indx,
+       double *d);
+
+extern void
+lineqsolve(const int n, const int nrhs,
+	   double *a,
+	   double *b);
