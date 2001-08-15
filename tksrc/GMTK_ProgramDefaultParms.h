02bd545feb8ec623f4e1fb4bb66358d324a2a24c
Jeff Bilmes <bilmes@users.noreply.github.com>
2001-08-15 15:08:17 +0000

    Initial revision

---
+++ tksrc/GMTK_ProgramDefaultParms.h
@@ -0,0 +1,44 @@
+/*
+ * "Copyright 2001, University of Washington and International Business Machines Corporation. All Rights Reserved
+ *
+ *    Written by Jeff Bilmes and Geoffrey Zweig
+ *
+ * NO WARRANTY
+ * THE PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT
+ * LIMITATION, ANY WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Each Recipient is
+ * solely responsible for determining the appropriateness of using the Program
+ * and assumes all risks associated with such use, including but not limited
+ * to the risks and costs of program errors, compliance with applicable laws,
+ * damage to or loss of data, programs or equipment, and unavailability or
+ * interruption of operations.
+
+ * DISCLAIMER OF LIABILITY
+ * THE UNIVERSITY OF WASHINGTON, INTERNATIONAL BUSINESS MACHINES CORPORATION,
+ * JEFF BILMES AND GEOFFREY ZWEIG SHALL NOT HAVE ANY LIABILITY FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING WITHOUT LIMITATION LOST PROFITS), HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  OF
+ * THE PROGRAM, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES."
+*/
+
+/*-
+ * GMTK_ProgramDefaultParms.h
+ *
+ *  Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ *    The Default parameters by all programs.
+ * 
+ *  $Header$
+ */
+
+
+#ifndef GMTK_PROGRAMDEFAULTPARMS_H
+#define GMTK_PROGRAMDEFAULTPARMS_H
+
+#define GMTK_DEFAULT_VARIANCE_FLOOR (1e-10)
+#define GMTK_DEFAULT_NUM_SPLITS (3)
+#define GMTK_DEFAULT_BASECASETHRESHOLD (1000)
+
+#endif
