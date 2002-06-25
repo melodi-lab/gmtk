30e399d940ab8dcc8b03e3752607e88ed2d3d4ed
Jeff Bilmes <bilmes@users.noreply.github.com>
2002-06-25 18:49:46 +0000

    Initial revision

---
+++ IEEEFloatingpoint/ieeeFPsetup_cygwin.cc
@@ -0,0 +1,38 @@
+//
+//
+// Setup code so that FPE's print out not only that an FPE occured
+// but what kind of FPE occured. This code is very specific to
+// Cygwin.
+// 
+// Written by: Jeff Bilmes
+//             bilmes@ee.washington.edu
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+#include <signal.h>
+#include <sys/signal.h>
+
+#include "general.h"
+#include "ieeeFPsetup.h"
+#include "error.h"
+
+
+VCID("$Header$");
+
+#ifdef HAVE_NONSTANDARD_ARITHMETIC
+
+#endif
+
+void ieeeFPsetup()
+{
+  // does not yet do anything for this architecture.
+}
+
+
+
+
+
