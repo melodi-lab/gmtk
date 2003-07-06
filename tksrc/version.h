34ea48866161b4546c63ed839586aa9e7271ae93
bilmes <bilmes@localhost>
2003-07-06 04:28:59 +0000

    Initial revision

---
+++ tksrc/version.h
@@ -0,0 +1,24 @@
+/*
+ * GMTK_GM.h
+ * Parses a text file giving the basic GM structure
+ * over hidden varialbes.
+ *
+ * Written by Jeff Bilmes <bilmes@ee.washington.edu>
+ *
+ * Copyright (c) 2001, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle make no representations about the suitability of this software
+ * for any purpose. It is provided "as is" without express or implied warranty.
+ *
+ *
+ * $Header$
+ *
+ */
+
+static char * gmtk_version_id = "GMTK Version 0.2a Sat Jul  5 21:28:33 2003";
+
+
