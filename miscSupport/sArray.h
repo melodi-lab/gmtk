80beffb06155a9da49fa8e614ee9c919f135386c
Jeff Bilmes <bilmes@users.noreply.github.com>
2003-09-17 02:16:53 +0000

    Updated to support for new hash table structures

--- miscSupport/sArray.h
+++ miscSupport/sArray.h
@@ -1,21 +1,26 @@
 //
 //
-// sArray: a Simple Array class that is basically a glorified 
-// type*.
+// sArray: a Simple Array class that is basically a glorified type*.
 // 
-// This class is NOT meant for protection. It is
-// mean to have type* (such as an int*, float*, double*, etc.)
-// and where the underlying pointer is easy to access for
-// low level loops. But this class also provides
-// for convenient managing of lengths, allocation, deallocation,
-// and resizing.
+// This class is NOT meant for protection. It is mean to have type*
+// (such as an int*, float*, double*, etc.)  and where the underlying
+// pointer is easy to access for low level loops. But this class also
+// provides for convenient managing of lengths, allocation,
+// deallocation, and resizing.
 //
-// ****** NOTE ***** There is no COPY CONSTRUCTOR.
-
+// ******************* NOTE *****************************************
+// There is no COPY CONSTRUCTOR, so all copies are bit-wise
+// copies. This means that sArrays work *COMPLETELY* differently then
+// say STL vector types, etc.. sArrays are meant to be used when you
+// wish not to create lots of temporaries during
+// construction/destruction in routine return and call.
+// ******************* NOTE *****************************************
+// 
+//
 // $Header$
 //
 // Written by: Jeff Bilmes
-//             bilmes@icsi.berkeley.edu
+//             bilmes@ee.washington.edu
 //
 
 #ifndef SARRAY_H
@@ -29,132 +34,142 @@
 template <class T>
 class sArray {
 
-  int size;
+  int _size;
 
  public:
 
   T *ptr;
 
-  sArray(int _size=0) {
-    size = _size;
+  sArray(int arg_size=0) {
+    _size = arg_size;
     ptr = NULL;
-    if (size < 0)
-      error("Error: sArray::sArray _size < 0");
-    if (size > 0)
-      ptr = new T[size];
+    if (_size < 0)
+      error("Error: sArray::sArray arg_size < 0");
+    if (_size > 0)
+      ptr = new T[_size];
   }
 
   ~sArray() {
     delete [] ptr;
   }
-  void resize(int _size) {
-    if (_size < 0)
-      error("Error: Sarray:resize _size < 0");
+  void resize(int arg_size) {
+    if (arg_size < 0)
+      error("Error: Sarray:resize arg_size < 0");
     delete [] ptr;
-    size = _size;
-    ptr = new T[size];
+    _size = arg_size;
+    ptr = new T[_size];
   }
-  void resizeIfDifferent(int _size) {
-    if (_size != size)
-      resize(_size);
+  void resizeIfDifferent(int arg_size) {
+    if (arg_size != _size)
+      resize(arg_size);
   }
-  void growIfNeeded(const int _size) {
-    if (_size > size)
-      resize(_size);
+  void growIfNeeded(const int arg_size) {
+    if (arg_size > _size)
+      resize(arg_size);
   }
 
-  void growByNIfNeeded(const int n,const int _size) {
+  void growByNIfNeeded(const int n,const int arg_size) {
     assert ( n >= 1 );
-    if (_size > size)
-      resize(n*_size);
+    if (arg_size > _size)
+      resize(n*arg_size);
   }
 
-  void growByFIfNeeded(const float f,const int _size) {
+  void growByFIfNeeded(const float f,const int arg_size) {
     assert ( f >= 1.0 );
-    if (_size > size)
-      resize((int)(f*_size+1.0));
+    if (arg_size > _size)
+      resize((int)(f*arg_size+1.0));
   }
 
-  void resizeAndCopy(const int _size) {
-    if (_size < 0)
-      error("Error: Sarray:resize _size < 0");
-    T* tmp = new T[_size];
-    const int nsize = (size<_size?size:_size);
+  void resizeAndCopy(const int arg_size) {
+    if (arg_size < 0)
+      error("Error: Sarray:resize arg_size < 0");
+    T* tmp = new T[arg_size];
+    const int nsize = (_size<arg_size?_size:arg_size);
     ::memcpy((void*)tmp,(void*)ptr,sizeof(T)*nsize);
     delete [] ptr;
-    size = _size;
+    _size = arg_size;
     ptr = tmp;
   }
 
-  void growIfNeededAndCopy(int _size) {
-    if (_size > size)
-      resizeAndCopy(_size);
+  void growIfNeededAndCopy(int arg_size) {
+    if (arg_size > _size)
+      resizeAndCopy(arg_size);
   }
 
-  void growByNIfNeededAndCopy(const int n,int _size) {
-    if (_size > size)
-      resizeAndCopy(n*_size);
+  void growByNIfNeededAndCopy(const int n,int arg_size) {
+    if (arg_size > _size)
+      resizeAndCopy(n*arg_size);
   }
 
-  void growByFIfNeededAndCopy(const float f,int _size) {
+  void growByFIfNeededAndCopy(const float f,int arg_size) {
     assert ( f >= 1.0 );
-    if (_size > size)
-      resizeAndCopy((int)(f*_size+1.0));
+    if (arg_size > _size)
+      resizeAndCopy((int)(f*arg_size+1.0));
   }
 
   void swapPtrs(sArray<T>& sa) {
-    if (size != sa.size)
+    if (_size != sa._size)
       error("Error: Sarray:swapPtrs, different sizes");
     T *tmp = ptr;
     ptr = sa.ptr;
     sa.ptr = tmp;
   }
 
+
+  void swap(sArray<T>& sa) {
+    T *tmp = ptr;
+    ptr = sa.ptr;
+    sa.ptr = tmp;
+    int tmp_size = _size;
+    _size = sa._size;
+    sa._size = tmp_size;
+  }
+
   // Append array y to the end of this one
   void concatenate(sArray<T>& y)
   {
     // Resize ourselves
-    if (y.size <= 0)
+    if (y._size <= 0)
       return;
 
-    int oldsize = size;
-    resizeAndCopy(size + y.size);
-    ::memcpy((void*)(ptr+oldsize),(void*)y.ptr,sizeof(T)*y.size);
+    int oldsize = _size;
+    resizeAndCopy(_size + y._size);
+    ::memcpy((void*)(ptr+oldsize),(void*)y.ptr,sizeof(T)*y._size);
 
   }
     
   inline void clear() {
     delete [] ptr;
     ptr = NULL;
-    size = 0;
+    _size = 0;
   }
 
   inline bool contains(T& x)
   {
-    for (int i=0; i<size; i++)
+    for (int i=0; i<_size; i++)
       if (ptr[i] == x)
         return true;
     return false;
   }
     
-  inline int len() const { return size; }
+  inline int len() const { return _size; }
+  inline unsigned size() const { return (unsigned)_size; }
   inline T& operator[](const int i) { 
-    assert ( i >= 0 && i < size );
+    assert ( i >= 0 && i < _size );
     return ptr[i]; 
   }
   inline T operator[] (const int i) const { 
-    assert ( i >= 0 && i < size );
+    assert ( i >= 0 && i < _size );
     return ptr[i]; 
   }
   
   void assignAllToValue(const T x) {
-    for (int i=0; i<size; i++)
+    for (int i=0; i<_size; i++)
       ptr[i] = x;
   }
 
 
 };
-
 
 
 #endif
