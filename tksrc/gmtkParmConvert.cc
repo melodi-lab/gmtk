0005caff778ec439f0c4df4561f338ce35fd850a
bilmes <bilmes@localhost>
2004-03-07 08:33:30 +0000

    - fixed problem with global collection for rest of programs as well .

--- tksrc/gmtkParmConvert.cc
+++ tksrc/gmtkParmConvert.cc
@@ -201,6 +201,7 @@ main(int argc,char*argv[])
     else
       error("Error: command line argument '-allocateDenseCpts d', must have d = {0,1,2}\n");
   }
+  GM_Parms.finalizeParameters();  
 
   printf("Finished reading in all parameters and structures\n");
   printf("Total number of trainable parameters in input files = %u\n",
