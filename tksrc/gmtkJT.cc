52c7e5d8f3f7e0b835b7925bb8abdb980de7ebf4
Jeff Bilmes <bilmes@users.noreply.github.com>
2004-07-05 07:41:57 +0000

    updated infoMsg flags.

--- tksrc/gmtkJT.cc
+++ tksrc/gmtkJT.cc
@@ -136,13 +136,21 @@ Arg Arg::Args[] = {
   Arg("fmt1",Arg::Opt,fmts[0],"Format (htk,bin,asc,pfile) for observation file 1"),
   Arg("iswp1",Arg::Opt,iswps[0],"Endian swap condition for observation file 1"),
 
-  Arg("of2",Arg::Opt,ofs[1],"Observation File 1"),
-  Arg("nf2",Arg::Opt,nfs[1],"Number of floats in observation file 1"),
-  Arg("ni2",Arg::Opt,nis[1],"Number of ints in observation file 1"),
-  Arg("fr2",Arg::Opt,frs[1],"Float range for observation file 1"),
-  Arg("ir2",Arg::Opt,irs[1],"Int range for observation file 1"),
-  Arg("fmt2",Arg::Opt,fmts[1],"Format (htk,bin,asc,pfile) for observation file 1"),
-  Arg("iswp2",Arg::Opt,iswps[1],"Endian swap condition for observation file 1"),
+  Arg("of2",Arg::Opt,ofs[1],"Observation File 2"),
+  Arg("nf2",Arg::Opt,nfs[1],"Number of floats in observation file 2"),
+  Arg("ni2",Arg::Opt,nis[1],"Number of ints in observation file 2"),
+  Arg("fr2",Arg::Opt,frs[1],"Float range for observation file 2"),
+  Arg("ir2",Arg::Opt,irs[1],"Int range for observation file 2"),
+  Arg("fmt2",Arg::Opt,fmts[1],"Format (htk,bin,asc,pfile) for observation file 2"),
+  Arg("iswp2",Arg::Opt,iswps[1],"Endian swap condition for observation file 2"),
+
+  Arg("of3",Arg::Opt,ofs[2],"Observation File 3"),
+  Arg("nf3",Arg::Opt,nfs[2],"Number of floats in observation file 3"),
+  Arg("ni3",Arg::Opt,nis[2],"Number of ints in observation file 3"),
+  Arg("fr3",Arg::Opt,frs[2],"Float range for observation file 3"),
+  Arg("ir3",Arg::Opt,irs[2],"Int range for observation file 3"),
+  Arg("fmt3",Arg::Opt,fmts[2],"Format (htk,bin,asc,pfile) for observation file 3"),
+  Arg("iswp3",Arg::Opt,iswps[2],"Endian swap condition for observation file 3"),
 
 
   /////////////////////////////////////////////////////////////
@@ -452,9 +460,9 @@ main(int argc,char*argv[])
 
     } else {
       unsigned numUsableFrames = myjt.unroll(numFrames);
-      infoMsg(IM::Default,"Collecting Evidence\n");
+      infoMsg(IM::Low,"Collecting Evidence\n");
       myjt.collectEvidence();
-      infoMsg(IM::Default,"Done Collecting Evidence\n");
+      infoMsg(IM::Low,"Done Collecting Evidence\n");
       logpr probe = myjt.probEvidence();
       printf("Segment %d, after CE, log(prob(evidence)) = %f, per frame =%f, per numUFrams = %f\n",
 	     segment,
@@ -463,12 +471,12 @@ main(int argc,char*argv[])
 	     probe.val()/numUsableFrames);
 
       if (doDistributeEvidence) {
-	infoMsg(IM::Default,"Distributing Evidence\n");
+	infoMsg(IM::Low,"Distributing Evidence\n");
 	myjt.distributeEvidence();
-	infoMsg(IM::Default,"Done Distributing Evidence\n");
+	infoMsg(IM::Low,"Done Distributing Evidence\n");
 
 	if (JunctionTree::viterbiScore)
-	  infoMsg(IM::Default,"Note: Clique sums will be different since viteri option is active\n");
+	  infoMsg(IM::SoftWarning,"Note: Clique sums will be different since viteri option is active\n");
 	myjt.printAllCliquesProbEvidence();
 	
 	probe = myjt.probEvidence();
