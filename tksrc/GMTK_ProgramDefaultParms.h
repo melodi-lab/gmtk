72f532f4da04c4f98f5ce5c42ce67c5ba37bf770
Jeff Bilmes <bilmes@users.noreply.github.com>
2005-03-01 10:11:59 +0000

    accurate header since entirely written by bilmes

--- tksrc/GMTK_ProgramDefaultParms.h
+++ tksrc/GMTK_ProgramDefaultParms.h
@@ -1,29 +1,3 @@
-/*
- * "Copyright 2001, University of Washington and International Business Machines Corporation. All Rights Reserved
- *
- *    Written by Jeff Bilmes and Geoffrey Zweig
- *
- * NO WARRANTY
- * THE PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED INCLUDING, WITHOUT
- * LIMITATION, ANY WARRANTIES OR CONDITIONS OF TITLE, NON-INFRINGEMENT,
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Each Recipient is
- * solely responsible for determining the appropriateness of using the Program
- * and assumes all risks associated with such use, including but not limited
- * to the risks and costs of program errors, compliance with applicable laws,
- * damage to or loss of data, programs or equipment, and unavailability or
- * interruption of operations.
-
- * DISCLAIMER OF LIABILITY
- * THE UNIVERSITY OF WASHINGTON, INTERNATIONAL BUSINESS MACHINES CORPORATION,
- * JEFF BILMES AND GEOFFREY ZWEIG SHALL NOT HAVE ANY LIABILITY FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING WITHOUT LIMITATION LOST PROFITS), HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE  OF
- * THE PROGRAM, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGES."
-*/
-
 /*-
  * GMTK_ProgramDefaultParms.h
  *
