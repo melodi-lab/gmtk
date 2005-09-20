173ded0100eff2b295270533351a375d656e01eb
gang <gang@localhost>
2005-09-20 21:00:24 +0000

    new design of lattice CPT interation logic.  The new logic will prune away assumptions where time constrain violates as soon as possible now.

--- tksrc/GMTK_LatticeNodeCPT.cc
+++ tksrc/GMTK_LatticeNodeCPT.cc
@@ -33,9 +33,11 @@
  *-----------------------------------------------------------------------
  */
 LatticeNodeCPT::LatticeNodeCPT() : CPT(di_LatticeNodeCPT) , _latticeAdt(NULL) {
-	// some values are fixed
-	_numParents = 1;
-	cardinalities.resize(1);
+  // some values are fixed
+  // the first parent must be previous node and second one must be
+  // word transition
+  _numParents = 2;
+  cardinalities.resize(2);
 }
 
 
@@ -65,81 +67,67 @@ LatticeNodeCPT::~LatticeNodeCPT() {
  *-----------------------------------------------------------------------
  */
 void LatticeNodeCPT::becomeAwareOfParentValuesAndIterBegin(vector< RV* >& parents, iterator &it, DiscRV* drv, logpr& p) {
-	// For simplity reason, here cardinality of lattice nodes can be
-	// bigger than number of real nodes in some particular lattice.
-	// This is because in iterable lattices, different lattices can
-	// have different number of nodes.  But in master file, we can
-	// just specify the max of those.
-	if ( RV2DRV(parents[0])->val >= _latticeAdt->_end ) {
-		// This is a trick to make sure that lattice
-		// always reaches its end.
-		// The down side the the word variable in the last
-		// frame gives garbage.
-		it.internalState = _latticeAdt->_end;
-		it.internalStatePtr = NULL;
-		it.drv = drv;
-		drv->val = _latticeAdt->_end;
-		p.set_to_one();
-		return;
-	}
+  // For simplity reason, here cardinality of lattice nodes can be
+  // bigger than number of real nodes in some particular lattice.
+  // This is because in iterable lattices, different lattices can
+  // have different number of nodes.  But in master file, we can
+  // just specify the max of those.
+  if ( RV2DRV(parents[0])->val > _latticeAdt->_end ) {
+    it.internalStatePtr = NULL;
+    p.set_to_zero();
+    return;
+  }
 
-	LatticeADT::LatticeNode &node = _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val];
+  // case on the current frame index
+  if ( drv->frame() < _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].startFrame ) {
+    if ( RV2DRV(parents[1])->val ) {
+      // word transition not allowed
+      it.internalStatePtr = NULL;
+      p.set_to_zero();
+    } else {
+      // no word transition, just copy values from previous frame
+      it.internalStatePtr = NULL;
+      drv->val = RV2DRV(parents[0])->val;
+      p.set_to_one();
+    }
+  } else if ( drv->frame() > _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].endFrame ) {
+    // we force this cannot happen by setting prob to zero
+    it.internalStatePtr = NULL;
+    p.set_to_zero();
+  } else {
+    if ( RV2DRV(parents[1])->val ) {
+      // iterate next lattice nodes
+      // find the out going edge
+      LatticeADT::LatticeNode &node = _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val];
 
-	// if the lattice node is the end, return prob zero
-	if ( node.edges.totalNumberEntries() == 0 ) {
-		p.set_to_zero();
-		return;
-	}
+      // if the lattice node is the end, return prob zero
+      if ( node.edges.totalNumberEntries() == 0 ) {
+	it.internalStatePtr = NULL;
+	p.set_to_zero();
+	return;
+      }
 
-	// check out the out-going edges based on parent value
-	shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator *pit = new shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator();
+      // check out the out-going edges based on parent value
+      shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator *pit = new shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator();
 
-	// now the current node can have next transition
-	// find the correct iterators
-	node.edges.begin(*pit);
+      // now the current node can have next transition
+      // find the correct iterators
+      node.edges.begin(*pit);
 
-	// check the frame number and parent frame index to see whether
-	// transition is allowed at this frame
-	if ( drv->frame() >= _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].startFrame
-			&& drv->frame() <= _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].endFrame ) {
-	//if ( drv->frame() >= _latticeAdt->_latticeNodes[pit->key()].startFrame
-	//		&& drv->frame() <= _latticeAdt->_latticeNodes[pit->key()].endFrame ) {
-		// easy case
-		// set up the iternal state for iterators
-		it.internalStatePtr = (void*)pit;
-		it.internalState = RV2DRV(parents[0])->val;
-		it.drv = drv;
+      // set up the internal state for iterator
+      it.internalStatePtr = (void*)pit;
+      it.internalState = RV2DRV(parents[0])->val;
+      it.drv = drv;
 
-		// set up the values
-		drv->val = pit->key();
-		p = (**pit).prob_score;		// TODO: implement other scores
-
-		return;
-	} else {
-		// need to find the next available in the tree
-		while ( pit->next() ) {
-			if ( drv->frame() >= _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].startFrame && drv->frame() <= _latticeAdt->_latticeNodes[RV2DRV(parents[0])->val].endFrame ) {
-			//if ( drv->frame() >= _latticeAdt->_latticeNodes[pit->key()].startFrame && drv->frame() <= _latticeAdt->_latticeNodes[pit->key()].endFrame ) {
-				// this is what we need
-				// set up the iternal state for iterators
-				it.internalStatePtr = (void*)pit;
-				it.internalState = RV2DRV(parents[0])->val;
-				it.drv = drv;
-
-				// set up the values
-				drv->val = pit->key();
-				p = (**pit).prob_score;		// TODO: implement other scores
-
-				return;
-			}
-		}
-
-		// we didn't find anything satisfying the frame constrain
-		delete pit;
-		it.internalStatePtr = NULL;
-		p.set_to_zero();
-		return;
-	}
+      drv->val = pit->key();
+      p = (**pit).prob_score;
+    } else {
+      // no word transition, copy value
+      it.internalStatePtr = NULL;
+      drv->val = RV2DRV(parents[0])->val;
+      p.set_to_one();
+    }
+  }
 }
 
 
@@ -178,38 +166,28 @@ logpr LatticeNodeCPT::probGivenParents(vector< RV* >& parents, DiscRV* drv) {
  *-----------------------------------------------------------------------
  */
 bool LatticeNodeCPT::next(iterator &it, logpr& p) {
-	shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator* pit = (shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator*) it.internalStatePtr;
+  shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator* pit = (shash_map_iter<unsigned, LatticeADT::LatticeEdge>::iterator*) it.internalStatePtr;
 
-	// check whether pit is null
-	// althout this shouldn't happen
-	// 1. if the end user didn't check prob value in begin
-	//    the system will not know that next shouldn't be called
-	if ( pit == NULL ) {
-		// theoretically, this should throw an error
-		// but the way inference engine is designed now, this case
-		// might still happen
-		p.set_to_zero();
-		return false;
-		//error("Error: LatticeNodeCPT::next shouldn't be called when begin returns a zero probability");
-	}
+  // check whether pit is null
+  if ( pit == NULL ) {
+    p.set_to_zero();
+    return false;
+  }
 
-	// need to find the next available in the tree
-	while ( pit->next() ) {
-		if ( it.drv->frame() >= _latticeAdt->_latticeNodes[it.internalState].startFrame && it.drv->frame() <= _latticeAdt->_latticeNodes[it.internalState].endFrame ) {
-		//if ( it.drv->frame() >= _latticeAdt->_latticeNodes[pit->key()].startFrame && it.drv->frame() <= _latticeAdt->_latticeNodes[pit->key()].endFrame ) {
-			// set up the values
-			it.drv->val = pit->key();
-			p = (**pit).prob_score;		// TODO: implement other scores
+  // find the next available in the tree
+  if ( pit->next() ) {
+    // set up the values
+    it.drv->val = pit->key();
+    p = (**pit).prob_score;		// TODO: implement other scores
 
-			return true;
-		}
-	}
-
-	// we didn't find anything satisfying the frame constrain
-	delete pit;
-	it.internalStatePtr = NULL;
-	p.set_to_zero();
-	return false;
+    return true;
+  } else {
+    // we didn't find anything satisfying the frame constrain
+    delete pit;
+    it.internalStatePtr = NULL;
+    p.set_to_zero();
+    return false;
+  }
 }
 
 
@@ -223,7 +201,9 @@ bool LatticeNodeCPT::next(iterator &it, logpr& p) {
  *-----------------------------------------------------------------------
  */
 void LatticeNodeCPT::setLatticeADT(const LatticeADT &latticeAdt) {
-	_latticeAdt = &latticeAdt;
-	_card = cardinalities[0] = _latticeAdt->_nodeCardinality;
+  _latticeAdt = &latticeAdt;
+  // first parent is noade
+  _card = cardinalities[0] = _latticeAdt->_nodeCardinality;
+  // second parent is word transition
+  cardinalities[1] = 2;
 }
-
