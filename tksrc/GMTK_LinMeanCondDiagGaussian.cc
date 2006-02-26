7f8c37e4ae1c0bafa56131024481c2c8b3fbb3ca
sking <sking@localhost>
2006-02-26 00:24:59 +0000

    improved comments

--- tksrc/GMTK_LinMeanCondDiagGaussian.cc
+++ tksrc/GMTK_LinMeanCondDiagGaussian.cc
@@ -140,7 +140,25 @@ LinMeanCondDiagGaussian::write(oDataStreamFile& os)
   os.nl();
 }
 
-
+/*-
+ *-----------------------------------------------------------------------
+ * LinMeanCondDiagGaussian::makeRandom
+ *      calls makeRandom for the mean,covar and dlinks
+ *
+ * Preconditions:
+ *      Object must be allocated.
+ *
+ * Postconditions:
+ *      Object has random values.
+ *
+ * Side Effects:
+ *      destroys previous values.
+ *
+ * Results:
+ *      none
+ *
+ *-----------------------------------------------------------------------
+ */
 void
 LinMeanCondDiagGaussian::makeRandom()
 {
@@ -153,7 +171,25 @@ LinMeanCondDiagGaussian::makeRandom()
   dLinkMat->makeRandom();
 }
 
-
+/*-
+ *-----------------------------------------------------------------------
+ * LinMeanCondDiagGaussian::makeUniform
+ *      calls makeUniform for the mean,covar and dlinks
+ *
+ * Preconditions:
+ *      Object must be allocated.
+ *
+ * Postconditions:
+ *      Object has uniform values.
+ *
+ * Side Effects:
+ *      destroys previous values.
+ *
+ * Results:
+ *      none
+ *
+ *-----------------------------------------------------------------------
+ */
 void
 LinMeanCondDiagGaussian::makeUniform()
 {
@@ -1070,7 +1106,28 @@ void LinMeanCondDiagGaussian::sampleGenerate(float *const sample,
 
 
 
-
+/*-
+ *-----------------------------------------------------------------------
+ * LinMeanCondDiagGaussian::identicalIndependentClone
+ *      creates an exact copy of this object that shares nothing with
+ *      the original
+ *
+ * Preconditions:
+ *      1) object being copied should be allocated
+ *      2) GM_Parms should contain all parameters, so that a unique name
+ *         for the new object can be generated
+ *
+ * Postconditions:
+ *      none
+ *
+ * Side Effects:
+ *      the new object is added to GM_Parms
+ *
+ * Results:
+ *      a pointer the new object
+ *
+ *-----------------------------------------------------------------------
+ */
 Component*
 LinMeanCondDiagGaussian::identicalIndependentClone()
 {
