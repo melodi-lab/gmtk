b73b56eb4b0bd2d40206fba00d5507b5498b7a6a
rprogers <rprogers@localhost>
2010-08-25 00:52:42 +0000

    added, refs #2

---
+++ IEEEFloatingpoint/ieeeFPsetup_ANY_ANY.cc
@@ -0,0 +1,35 @@
+//
+//
+// Setup code so that FPE's print out not only that an FPE occured
+// but what kind of FPE occured. This code is very specific to
+// CPU and OS.
+// 
+// Written by: Jeff Bilmes
+//             bilmes@ee.washington.edu
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+
+#include <signal.h>
+#include <sys/signal.h>
+
+#include "general.h"
+//#include "ieeeFPsetup.h"
+#include "error.h"
+
+
+VCID("$Header$")
+
+
+void ieeeFPsetup()
+{
+  // does not yet do anything for this architecture.
+}
+
+
+
+
+
