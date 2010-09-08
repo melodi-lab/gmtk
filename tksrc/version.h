59990f65707f734e72a9a4e6f2ad1134354c8418
rprogers <rprogers@localhost>
2010-09-08 21:17:54 +0000

    use autotools provided version; refs #48

--- tksrc/version.h
+++ tksrc/version.h
@@ -18,8 +18,14 @@
  *
  */
 
+#if HAVE_CONFIG_H
+#include <config.h>
+static const char * gmtk_version_id = PACKAGE_VERSION;
+#else
 // TODO: automate the process of updating this string.
 static const char * gmtk_version_id = "GMTK Version 0.2b Tue Jan 20 22:59:41 2004";
+#endif
+
 // TODO: figure out a better way to keep compiler from complaining
 static const char *___gvi_tmp___ = gmtk_version_id;
 
