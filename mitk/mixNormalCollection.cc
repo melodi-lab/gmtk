d5d2c1a2576a1a1a6671c7e3fe93686f82395fa2
Richard Rogers <rprogers@uw.edu>
2010-09-20 16:37:39 -0700

    added check for {{{math.h}}} and conditionally include {{{config.h}}}, {{{math.h}}}, and {{{values.h}}} to make Linux builds happy; refs #2

--- mitk/mixNormalCollection.cc
+++ mitk/mixNormalCollection.cc
@@ -1,5 +1,16 @@
 #include <iostream>
 
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#if HAVE_MATH_H
+#include <math.h>
+#endif
+
+#if HAVE_VALUES_H
+#include <values.h>
+#endif
 
 
 #include "mixNormalCollection.h"
