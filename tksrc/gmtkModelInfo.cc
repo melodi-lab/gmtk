b123c84947b0827b9e84e5b2b8e195c76f445cf2
Richard Rogers <rprogers@uw.edu>
2011-04-11 18:42:52 -0700

    initial work on {{{gmtkModelInfo}}}; refs #131

---
+++ tksrc/gmtkModelInfo.cc
@@ -0,0 +1,255 @@
+/*
+ * gmtkModelInfo.cc
+ * produce a junction tree
+ *
+ * Written by Richard Rogers <rprogers@ee.washington.edu>
+ *
+ * Copyright (c) 2011, < fill in later >
+ *
+ * Permission to use, copy, modify, and distribute this
+ * software and its documentation for any non-commercial purpose
+ * and without fee is hereby granted, provided that the above copyright
+ * notice appears in all copies.  The University of Washington,
+ * Seattle make no representations about the suitability of this software
+ * for any purpose. It is provided "as is" without express or implied warranty.
+ *
+ */
+
+/*
+ * This program converts from ascii trainable parameters to binary
+ * and vice versa.
+ *
+ *
+ */
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <float.h>
+#include <assert.h>
+
+#include "general.h"
+#include "error.h"
+#include "rand.h"
+#include "arguments.h"
+#include "ieeeFPsetup.h"
+//#include "spi.h"
+#include "version.h"
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+#if HAVE_HG_H
+#include "hgstamp.h"
+#endif
+VCID(HGID)
+
+
+#include "GMTK_FileParser.h"
+#include "GMTK_RV.h"
+#include "GMTK_DiscRV.h"
+#include "GMTK_ContRV.h"
+#include "GMTK_GMParms.h"
+#include "GMTK_GMTemplate.h"
+#include "GMTK_Partition.h"
+#include "GMTK_ObservationMatrix.h"
+#include "GMTK_MixtureCommon.h"
+#include "GMTK_GaussianComponent.h"
+#include "GMTK_MeanVector.h"
+#include "GMTK_DiagCovarVector.h"
+#include "GMTK_DlinkMatrix.h"
+
+#define GMTK_ARG_INPUT_MASTER_FILE_OPT_ARG
+#define GMTK_ARG_OUTPUT_MASTER_FILE
+#define GMTK_ARG_OUTPUT_TRAINABLE_PARAMS
+#define GMTK_ARG_INPUT_TRAINABLE_PARAMS
+#define GMTK_ARG_CPP_CMD_OPTS
+#define GMTK_ARG_VERB
+#define GMTK_ARG_ALLOC_DENSE_CPTS
+#define GMTK_ARG_SEED
+#define GMTK_ARG_VAR_FLOOR
+#define GMTK_ARG_STR_FILE_OPT_ARG
+#define GMTK_ARG_VAR_FLOOR
+#define GMTK_ARG_VERSION
+#define GMTK_ARG_VAR_FLOOR_ON_READ
+#define GMTK_ARG_CPT_NORM_THRES
+
+#define GMTK_ARGUMENTS_DEFINITION
+#include "GMTK_Arguments.h"
+#undef GMTK_ARGUMENTS_DEFINITION
+
+
+Arg Arg::Args[] = {
+
+
+#define GMTK_ARGUMENTS_DOCUMENTATION
+#include "GMTK_Arguments.h"
+#undef GMTK_ARGUMENTS_DOCUMENTATION
+
+
+
+  // final one to signal the end of the list
+  Arg()
+
+};
+
+/*
+ * definition of needed global arguments
+ */
+RAND rnd(false);
+GMParms GM_Parms;
+ObservationMatrix globalObservationMatrix;
+
+int
+main(int argc,char*argv[])
+{
+  ////////////////////////////////////////////
+  // set things up so that if an FP exception
+  // occurs such as an "invalid" (NaN), overflow
+  // or divide by zero, we actually get a FPE
+  ieeeFPsetup();
+
+  ////////////////////////////////////////////
+  // parse arguments
+  bool parse_was_ok = Arg::parse(argc,(char**)argv);
+  if(!parse_was_ok) {
+    Arg::usage(); exit(-1);
+  }
+
+
+#define GMTK_ARGUMENTS_CHECK_ARGS
+#include "GMTK_Arguments.h"
+#undef GMTK_ARGUMENTS_CHECK_ARGS
+
+
+  /////////////////////////////////////////////
+
+  if (strFileName == NULL || allocateDenseCpts == 0) {
+    // check this only if the structure file is not given or
+    // if we are not allocating Dense CPTs, since
+    // in that case there is no way the user could specify
+    // automatic allocation of such CPTs.
+    if ((inputMasterFile == NULL) && (inputTrainableParameters == NULL)) {
+      warning("ERROR: need to specify command line parameters inputMasterFile or inputTrainableParameters (or both) when no structure file is given");
+      Arg::usage();
+      error("");
+    }
+  }
+
+  ////////////////////////////////////////////
+  if (inputMasterFile != NULL) {
+    iDataStreamFile pf(inputMasterFile,false,true,cppCommandOptions);
+    GM_Parms.read(pf);
+  }
+  if (inputTrainableParameters != NULL) {
+    // flat, where everything is contained in one file
+    iDataStreamFile pf(inputTrainableParameters,binInputTrainableParameters,true,cppCommandOptions);
+    GM_Parms.readTrainable(pf);
+  }
+  GM_Parms.finalizeParameters();  
+
+
+  // load up the structure file as we might want
+  // it to allocate some Dense CPTs.
+  FileParser fp(strFileName,cppCommandOptions);
+  infoMsg(IM::Tiny,"Finished reading in all parameters and structures\n");
+    
+  // parse the file
+  infoMsg(IM::Max,"Parsing structure file...\n");
+  fp.parseGraphicalModel();
+  // create the rv variable objects
+  infoMsg(IM::Max,"Creating rv objects...\n");
+  fp.createRandomVariableGraph();
+
+  // Make sure that there are no directed loops in the graph.
+  infoMsg(IM::Max,"Checking template...\n");
+  fp.ensureValidTemplate();
+
+  // link the RVs with the parameters that are contained in
+  // the bn1_gm.dt file.
+  if (allocateDenseCpts == 0)
+    fp.associateWithDataParams(FileParser::noAllocate);
+  else if (allocateDenseCpts == 1)
+    fp.associateWithDataParams(FileParser::allocateRandom);
+  else if (allocateDenseCpts == 2)
+    fp.associateWithDataParams(FileParser::allocateUniform);
+  else
+    error("Error: command line argument '-allocateDenseCpts d', must have d = {0,1,2}\n");
+  
+  
+  printf("Finished reading in all parameters and structures\n");
+  printf("Total number of trainable parameters in input files = %u\n",
+	 GM_Parms.totalNumberParameters());
+  GM_Parms.markUsedMixtureComponents();
+  
+  
+  infoMsg(IM::Max,"Creating template...\n");
+  //  GMTemplate gm_template(fp);
+  
+  vector<RV*> unrolled_rvs;
+  map<RVInfo::rvParent, unsigned> unrolled_map;
+  fp.unroll(0, unrolled_rvs, unrolled_map);
+
+  // #rvs  #rvs in P C E  #Drvs  #Crvs  min card  max card  #hid  #obs  min Crv dim  max Crv dim  gr comp.  #frames P C E  min fr  max fr  sw par  sw wght  sym  dense  sparse  det  DT  ItDT  IDT  Fng  Ng  Lat  sparseG  l1  l2  exp  gamma  beta  VECPT  VESEP
+  
+  unsigned nRVsInP = 0;
+  unsigned nRVsInC = 0;
+  unsigned nRVsInE = 0;
+  unsigned nDiscRVs = 0;
+  unsigned nContRVs = 0;
+  unsigned nObsRVs = 0;
+  unsigned nHidRVs = 0;
+  unsigned maxRVcard = 0;
+  unsigned minRVcard = (unsigned)(-1);
+  unsigned maxRVdim = 0;
+  unsigned minRVdim = (unsigned)(-1);
+
+  for (vector<RV*>::iterator it = unrolled_rvs.begin(); it != unrolled_rvs.end(); ++it) {
+    if ((*it)->frame() < fp.firstChunkFrame()) {
+      nRVsInP += 1;
+    } else if ((*it)->frame() <= fp.lastChunkFrame()) {
+      nRVsInC += 1;
+    } else {
+      nRVsInE += 1;
+    }
+    if ((*it)->discrete()) {
+      nDiscRVs += 1;
+      unsigned card = ((DiscRV *)(*it))->cardinality;
+      if (card > maxRVcard) maxRVcard = card;
+      if (card < minRVcard) minRVcard = card;
+    } else if ((*it)->continuous()) {
+      nContRVs += 1;
+      unsigned dim = (unsigned) ((ContRV*)(*it))->dimensionality();
+      if (dim > maxRVdim) maxRVdim = dim;
+      if (dim < minRVdim) minRVdim = dim;
+    }
+    if ((*it)->hidden()) {
+      nHidRVs += 1;
+    } else if ((*it)->observed()) {
+      nObsRVs += 1;
+    }
+
+  }
+  if (nContRVs == 0) {
+    minRVdim = 0;
+  }
+  unsigned nRVs = unrolled_rvs.size();
+  printf("%u %u %u %u %u %u %u %u %u %u %u %u\n", nRVs, nRVsInP, nRVsInC, nRVsInE, nDiscRVs, nContRVs, minRVcard, maxRVcard, nHidRVs, nObsRVs, minRVdim, maxRVdim);
+  assert(nRVs = nRVsInP + nRVsInC + nRVsInE);
+  assert(nRVs = nObsRVs + nHidRVs);
+  assert(nRVs = nDiscRVs + nContRVs);
+
+  if (outputMasterFile != NULL) {
+    GM_Parms.write(outputMasterFile,cppCommandOptions);
+  }
+  if (outputTrainableParameters != NULL) {
+    oDataStreamFile of(outputTrainableParameters,binOutputTrainableParameters);
+    GM_Parms.writeTrainable(of);
+  }
+
+  exit_program_with_status(0);
+}
+
