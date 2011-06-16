e6352e6a8aefe4c22bcc726a27537d707b4bffce
Richard Rogers <rprogers@uw.edu>
2011-06-16 03:46:09 -0700

    removed {{{Range}}} checks from low-level routines - juggle level in {{{collectEvidence}}} and {{{distributedEvidence}}} instead; refs #109

--- tksrc/GMTK_JunctionTreeMessagePassing.cc
+++ tksrc/GMTK_JunctionTreeMessagePassing.cc
@@ -965,12 +965,6 @@ JunctionTree::ceGatherIntoRoot(PartitionStructures& ps,
 			       const bool clearWhenDone,
 			       const bool alsoClearOrigins)
 {
-  unsigned inferenceDebugLevel = IM::glbMsgLevel(IM::Inference);
-
-  if (! fdbugRange.contains((int)part_num)) {
-    IM::setGlbMsgLevel(IM::Inference, IM::Nothing);
-  }
-
   // first check that this is not an empty partition.
   if (ps.maxCliquesSharedStructure.size() == 0)
     return;
@@ -1024,11 +1018,6 @@ JunctionTree::ceGatherIntoRoot(PartitionStructures& ps,
   if (IM::messageGlb(IM::Inference, IM::Med+9)) {
     pt.reportMemoryUsageTo(ps,stdout);
   }
-
-  if (! fdbugRange.contains((int)part_num)) {
-    IM::setGlbMsgLevel(IM::Inference, inferenceDebugLevel);
-  }
-
 }
 
 
@@ -1091,19 +1080,18 @@ JunctionTree::ceSendForwardsCrossPartitions(// previous partition
   if (previous_ps.maxCliquesSharedStructure.size() == 0 || next_ps.maxCliquesSharedStructure.size() == 0)
     return;
 
-  if(fdbugRange.contains((int)previous_part_num))
-    infoMsg(IM::Inference, IM::Mod,"CE: message %s,part[%d],clique(%d) --> %s,part[%d],clique(%d)\n",
-	   previous_part_type_name,
-	   previous_part_num,
-	   previous_part_root,
-	   next_part_type_name,
-	   next_part_num,
-	   next_part_leaf);
+  infoMsg(IM::Inference, IM::Mod,"CE: message %s,part[%d],clique(%d) --> %s,part[%d],clique(%d)\n",
+	  previous_part_type_name,
+	  previous_part_num,
+	  previous_part_root,
+	  next_part_type_name,
+	  next_part_num,
+	  next_part_leaf);
   previous_pt.maxCliques[previous_part_root].
     ceSendToOutgoingSeparator(previous_ps.maxCliquesSharedStructure[previous_part_root],
 			      next_pt.separatorCliques[next_ps.separatorCliquesSharedStructure.size()-1],
 			      next_ps.separatorCliquesSharedStructure[next_ps.separatorCliquesSharedStructure.size()-1]);
-  if (fdbugRange.contains((int)previous_part_num) && IM::messageGlb(IM::Inference, IM::Med+9)) {
+  if (IM::messageGlb(IM::Inference, IM::Med+9)) {
     previous_pt.reportMemoryUsageTo(previous_ps,stdout);
   }
 }
@@ -1166,6 +1154,16 @@ JunctionTree::collectEvidence()
   // we assume the member does not have any dynamc sub-members.
   new (&inference_it) ptps_iterator(*this);
 
+  unsigned inferenceDebugLevel = IM::glbMsgLevel(IM::Inference);
+
+  if (! fdbugRange.contains((int)inference_it.pt_i())) {
+#if 0
+    printf("collectEvidence [part 0]: lowering inference level to %d\n", IM::glbMsgLevel(IM::DefaultModule));
+#endif
+    IM::setGlbMsgLevel(IM::Inference, IM::glbMsgLevel(IM::DefaultModule));
+  }
+
+
   init_CC_CE_rvs(inference_it);
 
   // we skip the first Co's LI separator if there is no P1
@@ -1183,7 +1181,23 @@ JunctionTree::collectEvidence()
   if (inference_it.at_first_c() && P1.cliques.size() == 0)
     Co.useLISeparator();
 
+
+  if (! fdbugRange.contains((int)inference_it.pt_i())) {
+#if 0
+    printf("CollectEvidence [part 0]: raising inference level to %d\n", inferenceDebugLevel);
+#endif
+    IM::setGlbMsgLevel(IM::Inference, inferenceDebugLevel);
+  }
+
   for (unsigned part=1;part < inference_it.pt_len(); part ++ ) {
+
+    if (! fdbugRange.contains((int)part)) {
+#if 0
+      printf("collectEvidence [part %u]: lowering inference level to %d\n", part, IM::glbMsgLevel(IM::DefaultModule));
+#endif
+      IM::setGlbMsgLevel(IM::Inference, IM::glbMsgLevel(IM::DefaultModule));
+    }
+
     setCurrentInferenceShiftTo(part);
 
     // inference_it.printState(stdout);
@@ -1217,6 +1231,14 @@ JunctionTree::collectEvidence()
 		     inference_it.pt_i());
     if (!inference_it.has_c_partition() && P1.cliques.size() == 0)
       E1.useLISeparator();
+
+    if (! fdbugRange.contains((int)part)) {
+#if 0
+      printf("CollectEvidence [part %u]: raising inference level to %d\n", part, inferenceDebugLevel);
+#endif
+      IM::setGlbMsgLevel(IM::Inference, inferenceDebugLevel);
+    }
+
   }
   assert ( inference_it.at_e() );
 
@@ -1346,8 +1368,7 @@ JunctionTree::deScatterOutofRoot(// the partition
   if (ps.maxCliquesSharedStructure.size() == 0)
     return;
 
-  if(fdbugRange.contains((int)part_num))
-    infoMsg(IM::Inference, IM::Med+5,"DE: distributing out of partition root %s,part[%d]: clique %d\n",
+  infoMsg(IM::Inference, IM::Med+5,"DE: distributing out of partition root %s,part[%d]: clique %d\n",
 	  part_type_name,part_num,root);
   pt.maxCliques[root].
     deScatterToOutgoingSeparators(ps.maxCliquesSharedStructure[root],
@@ -1356,16 +1377,14 @@ JunctionTree::deScatterOutofRoot(// the partition
   for (unsigned msgNoP1=message_order.size();msgNoP1 > 0; msgNoP1 --) {
     const unsigned to = message_order[msgNoP1-1].first;
     const unsigned from = message_order[msgNoP1-1].second;
-    if(fdbugRange.contains((int)part_num))
-      infoMsg(IM::Inference, IM::Mod,"DE: message %s,part[%d]: clique %d <-- clique %d\n",
+    infoMsg(IM::Inference, IM::Mod,"DE: message %s,part[%d]: clique %d <-- clique %d\n",
 	    part_type_name,part_num,to,from);
     pt.maxCliques[to].
       deReceiveFromIncommingSeparator(ps.maxCliquesSharedStructure[to],
 				      pt.separatorCliques,
 				      ps.separatorCliquesSharedStructure.ptr);
 
-      if(fdbugRange.contains((int)part_num))
-	infoMsg(IM::Inference, IM::Med+5,"DE: distributing out of %s,part[%d]: clique %d\n",
+    infoMsg(IM::Inference, IM::Med+5,"DE: distributing out of %s,part[%d]: clique %d\n",
 	    part_type_name,part_num,to);
     pt.maxCliques[to].
       deScatterToOutgoingSeparators(ps.maxCliquesSharedStructure[to],
@@ -1432,8 +1451,7 @@ JunctionTree::deSendBackwardsCrossPartitions(// previous partition
   if (previous_ps.maxCliquesSharedStructure.size() == 0 || next_ps.maxCliquesSharedStructure.size() == 0)
     return;
 
-  if(fdbugRange.contains((int)previous_part_num))
-    infoMsg(IM::Inference, IM::Mod,"DE: message %s,part[%d],clique(%d) <-- %s,part[%d],clique(%d)\n",
+  infoMsg(IM::Inference, IM::Mod,"DE: message %s,part[%d],clique(%d) <-- %s,part[%d],clique(%d)\n",
 	  previous_part_type_name,previous_part_num,previous_part_root,
 	  next_part_type_name,next_part_num,next_part_leaf);
   previous_pt.maxCliques[previous_part_root].
@@ -1496,8 +1514,17 @@ JunctionTree::deSendBackwardsCrossPartitions(// previous partition
 void
 JunctionTree::distributeEvidence()
 {
+  unsigned inferenceDebugLevel = IM::glbMsgLevel(IM::Inference);
 
   for (unsigned part= (inference_it.pt_len()-1) ; part > 0 ; part -- ) {
+
+    if (! fdbugRange.contains((int)part)) {
+#if 0
+      printf("distributeEvidence [part %u]: lowering inference level to %d\n", part, IM::glbMsgLevel(IM::DefaultModule));
+#endif
+      IM::setGlbMsgLevel(IM::Inference, IM::glbMsgLevel(IM::DefaultModule));
+    }
+
     setCurrentInferenceShiftTo(part);
     
     if (inference_it.at_first_c() && P1.cliques.size() == 0)
@@ -1532,16 +1559,38 @@ JunctionTree::distributeEvidence()
 				   inference_it.cur_nm(),
 				   inference_it.pt_i());
 
+    if (! fdbugRange.contains((int)part)) {
+#if 0
+      printf("distributedEvidence [part %u]: raising inference level to %d\n", part, inferenceDebugLevel);
+#endif
+      IM::setGlbMsgLevel(IM::Inference, inferenceDebugLevel);
+    }
+
   }
 
   setCurrentInferenceShiftTo(0);
   // do the final scatter out of root of initial P partition.
+
+  if (! fdbugRange.contains((int)inference_it.pt_i())) {
+#if 0
+    printf("distributeEvidence [0]: lowering inference level to %d\n", IM::glbMsgLevel(IM::DefaultModule));
+#endif
+    IM::setGlbMsgLevel(IM::Inference, IM::glbMsgLevel(IM::DefaultModule));
+  }
+
   deScatterOutofRoot(partitionStructureArray[inference_it.ps_i()],
 		     partitionTableArray[inference_it.pt_i()],
 		     inference_it.cur_ri(),
 		     inference_it.cur_message_order(),
 		     inference_it.cur_nm(),
 		     inference_it.pt_i());
+
+  if (! fdbugRange.contains((int)inference_it.pt_i())) {
+#if 0
+    printf("distributedEvidence [0]: raising inference level to %d\n", inferenceDebugLevel);
+#endif
+    IM::setGlbMsgLevel(IM::Inference, inferenceDebugLevel);
+  }
 
   if (viterbiScore)
     recordPartitionViterbiValue(inference_it);
