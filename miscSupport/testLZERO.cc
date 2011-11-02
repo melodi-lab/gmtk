466f1a654d97cdb608d073b036f819d0d3d19fc3
Richard Rogers <rprogers@uw.edu>
2011-11-02 14:20:54 -0700

    test for {{{0-0==0}}} too; refs #310

--- miscSupport/testLZERO.cc
+++ miscSupport/testLZERO.cc
@@ -10,7 +10,8 @@ main(int argc, char *argv[]) {
   logpr a(0.0);
   logpr b(0.0);
   logpr c = a + b;
-  if (c.zero())
+  logpr d = a - b;
+  if (c.zero() && d.zero())
     return 0;
   else
     return 5;
