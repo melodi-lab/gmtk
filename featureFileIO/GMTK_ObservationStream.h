4f8c4941cb5ad2b9a580e9391dcc359d5e293e03
Richard Rogers <rprogers@uw.edu>
2012-02-13 01:39:48 -0800

    added {{{FIXME}}}

--- featureFileIO/GMTK_ObservationStream.h
+++ featureFileIO/GMTK_ObservationStream.h
@@ -35,6 +35,9 @@
 //   FilterStream -   ObservationStream wrapper for IIR, ARMA, etc transforms
 //   FileStream   -   ObservationStream wrapper for seekable files (ObservationFile)
 
+
+// FIXME - handle properly the case where a stream ends mig-segment
+
 class ObservationStream {
 
  protected:
