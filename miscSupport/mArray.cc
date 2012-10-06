44b1dfc3961d726a4882803885266c30a18eeb22
Richard Rogers <rprogers@uw.edu>
2012-10-06 01:03:57 -0700

    Backed out changeset 7ddd352d4075

---
+++ miscSupport/mArray.cc
@@ -0,0 +1,98 @@
+//
+// A very simple array class.
+// This class is not meant for protection, it is only
+// for convenience.
+//
+// $Header$
+//
+// Written by: Jeff Bilmes
+//             bilmes@icsi.berkeley.edu
+
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <math.h>
+#include <limits.h>
+#include <float.h>
+#include <assert.h>
+
+#include "hgstamp.h"
+#include "general.h"
+VCID(HGID)
+#include "error.h"
+
+#include "mArray.h"
+
+unsigned   alloc_bytes = 0;
+unsigned dealloc_bytes = 0;
+
+#ifdef MAIN
+
+volatile int i;
+void func11() { i = 0x343434;} 
+
+
+#include "rand.h"
+unsigned short RAND::seedv[3];
+
+int
+main()
+{
+  mArray<int> iar;
+  mArray<float> far;
+
+  iar.resize(10);
+  far.resize(10);
+  // for (int i=0;i<10;i++) {
+  int i=0;
+  do {
+    func11();
+    iar[i] = i;
+    far[i] = i;
+  } while (++i < 10);
+  for (int i=0;i<10;i++) {
+    printf("iar[%d]=%d,far[%d]=%f\n",i,iar[i],i,far[i]);
+  }
+  iar.resizeAndCopy(5);
+  far.resizeAndCopy(5);
+  for (int i=0;i<5;i++) {
+    printf("iar[%d]=%d,far[%d]=%f\n",i,iar[i],i,far[i]);
+  }
+
+  mArray<mArray<float> > ffar;
+  ffar.resize(10);
+  for (int i=0;i<10;i++) {
+    ffar[i].resize(10);
+  }
+  for (int i=0;i<10;i++) {
+    for (int j=0;j<10;j++) {
+      ffar[i][j] = i*10-j;
+    }
+  }
+  for (int i=0;i<10;i++) {
+    for (int j=0;j<10;j++) {
+      ffar[i].sort();
+      printf("ffar[%d][%d] = %f\n",i,j,ffar[i][j]);
+    }
+  }
+  far.resize(100);
+
+  RAND rnd(true);
+  for (unsigned i=0;i<100;i++) {
+    far[i] = rnd.drand48();
+  } 
+  far.sort();
+  for (unsigned i=0;i<100;i++) {
+    if (i>0) {
+      assert( far[i] > far[i-1] );
+    }
+    printf("far[%d] = %f\n",i,far[i]);
+  }   
+
+}
+
+#endif
--- tksrc/gmtkViterbi.cc
+++ tksrc/gmtkViterbi.cc
@@ -132,7 +132,7 @@ VCID(HGID)
 #define GMTK_ARG_HASH_LOAD_FACTOR
 #define GMTK_ARG_STORE_DETERMINISTIC_CHILDREN
 #define GMTK_ARG_CLEAR_CLIQUE_VAL_MEM
-
+#define GMTK_ARG_USE_MMAP
 
 /****************************      FILE RANGE OPTIONS             ***********************************************/
 #define GMTK_ARG_FILE_RANGE_OPTIONS
@@ -165,6 +165,7 @@ VCID(HGID)
 #define GMTK_ARG_OBS_MATRIX_XFORMATION
 
 /************************            DECODING OPTIONS                  ******************************************/
+#define GMTK_VITERBI_FILE_WRITE
 #define GMTK_ARG_NEW_DECODING_OPTIONS
 
 // should be made conditional on having setrlimit available
@@ -369,6 +370,9 @@ main(int argc,char*argv[])
 
   logpr total_data_prob = 1.0;
 
+  if (JunctionTree::binaryViterbiFile && (pVitValsFileName || vitValsFileName)) {
+    error("Can't do both binary Viterbi output and -pVitValsFileName or -vitValsFileName");
+  }
   // What is the difference between the pVit* files and the vit*
   // files?  The pVit* files are similar to the vit* files, but the
   // pVit print via partitions, while the vit* does a bunch more
@@ -397,8 +401,8 @@ main(int argc,char*argv[])
 	error("Can't open file '%s' for writing\n",vitValsFileName);
     }
   }
-  if (!pVitValsFile && !vitValsFile) {
-    error("Argument Error: Missing REQUIRED argument: -pVitValsFile <str>  OR  -vitValsFile <str>\n");
+  if (!pVitValsFile && !vitValsFile && !JunctionTree::binaryViterbiFile) {
+    error("Argument Error: Missing REQUIRED argument: -pVitValsFile <str>  OR  -vitValsFile <str> OR -binaryViterbiFile <str>\n");
   }
 #endif
 
@@ -442,8 +446,53 @@ main(int argc,char*argv[])
     }
   }
 
+  if (JunctionTree::binaryViterbiFile) {
+    unsigned numSegments = globalObservationMatrix.numSegments();
+//printf("Writing %x segments\n", numSegments);
+    if (fwrite(&numSegments, sizeof(numSegments), 1, JunctionTree::binaryViterbiFile) != 1) {
+      char *err = strerror(errno);
+      error("Error writing to '%s': %s\n", JunctionTree::binaryViterbiFilename, err);
+    }
+    off_t off = (off_t) 0;
+    float score = 0.0/0.0; // initially nan so I can check that the index is written correctly (non-nan)
+    for (unsigned i=0; i < numSegments; i+=1) {
+      if (fwrite(&off, sizeof(off), 1, JunctionTree::binaryViterbiFile) != 1) {
+	char *err = strerror(errno);
+	error("Error writing to '%s': %s\n", JunctionTree::binaryViterbiFilename, err);
+      }
+      if (fwrite(&score, sizeof(score), 1, JunctionTree::binaryViterbiFile) != 1) {
+	char *err = strerror(errno);
+	error("Error writing to '%s': %s\n", JunctionTree::binaryViterbiFilename, err);
+      }
+    }
+    JunctionTree::nextViterbiOffset = ftello(JunctionTree::binaryViterbiFile);
+  }
+//printf("seg 0 starts @ %llx\n", ftello(JunctionTree::binaryViterbiFile));
   while (!dcdrng_it->at_end()) {
     const unsigned segment = (unsigned)(*(*dcdrng_it));
+
+    off_t indexOff;
+    off_t off;
+    float score;
+    if (JunctionTree::binaryViterbiFile) {
+      off = JunctionTree::nextViterbiOffset;
+      indexOff = (off_t) ( sizeof(unsigned) + segment * (sizeof(off_t) + sizeof(float)) );
+      if (fseeko(JunctionTree::binaryViterbiFile, indexOff, SEEK_SET)) {
+	char *err = strerror(errno);
+	error("Error seeking in '%s': %s\n", JunctionTree::binaryViterbiFilename, err);
+      }
+//printf("idx seg %03x -> %04llx @ %04llx = %llx\n", segment, off, indexOff, ftello(JunctionTree::binaryViterbiFile));
+      if (fwrite(&off, sizeof(off), 1, JunctionTree::binaryViterbiFile) != 1) {
+	char *err = strerror(errno);
+	error("Error writing to '%s': %s\n", JunctionTree::binaryViterbiFilename, err);
+      }
+      if (fseeko(JunctionTree::binaryViterbiFile, off, SEEK_SET)) {
+	char *err = strerror(errno);
+	error("Error seeking in '%s': %s\n", JunctionTree::binaryViterbiFilename, err);
+      }
+      JunctionTree::binaryViterbiOffset = off;
+    }
+
     if (globalObservationMatrix.numSegments() < (segment+1)) 
       error("ERROR: only %d segments in file, decode range must be in range [%d,%d] inclusive\n",
 	    globalObservationMatrix.numSegments(),
@@ -493,6 +542,19 @@ main(int argc,char*argv[])
 	infoMsg(IM::Inference, IM::Low,"Distributing Evidence\n");
 	myjt.distributeEvidence();
 	infoMsg(IM::Inference, IM::Low,"Done Distributing Evidence\n");
+      }
+    }
+
+    if (JunctionTree::binaryViterbiFile) {
+      indexOff = (off_t) ( sizeof(unsigned) + sizeof(off_t) + segment * (sizeof(off_t) + sizeof(float)) );
+      if (fseeko(JunctionTree::binaryViterbiFile, indexOff, SEEK_SET)) {
+	char *err = strerror(errno);
+	error("Error seeking in '%s': %s\n", JunctionTree::binaryViterbiFilename, err);
+      }
+      score = probe.val();
+      if (fwrite(&score, sizeof(score), 1, JunctionTree::binaryViterbiFile) != 1) {
+	char *err = strerror(errno);
+	error("Error writing to '%s': %s\n", JunctionTree::binaryViterbiFilename, err);
       }
     }
     
@@ -551,6 +613,8 @@ main(int argc,char*argv[])
   if (vitValsFile && vitValsFile != stdout)
     fclose(vitValsFile);
 #endif
+  if (JunctionTree::binaryViterbiFile)
+    fclose(JunctionTree::binaryViterbiFile);
 
   exit_program_with_status(0);
 }
