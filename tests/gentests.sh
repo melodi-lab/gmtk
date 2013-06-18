03273773bd664d3f4f4701e1d85e9272f4d14032
Richard Rogers <rprogers@uw.edu>
2013-06-18 14:39:06 -0700

    use {{{find}}} instead of {{{ls}}} to pick up more tests

--- tests/gentests.sh
+++ tests/gentests.sh
@@ -7,22 +7,29 @@ if [ $# != 1 ]; then
   exit 1
 fi
 
+TEST_AND_DEV=$1
 TAD=test_and_dev_scripts
 
-for d in `ls $1`; do
-  if test -x $1/$d/tricommand -a ! -f $TAD/test_and_dev_${d}.at; then
+for c in `find $TEST_AND_DEV -name tricommand`; do
+  ddd=${c%/tricommand}
+  dd=${ddd#$TEST_AND_DEV/}
+  d=`echo $dd | tr / _`
+
+  if test ! -f $TAD/test_and_dev_${d}.at; then
+    echo Generating tests for $dd
     cat <<EOF >$TAD/test_and_dev_${d}.at
 
-# test and dev model $d
+# test and dev model $dd
 
 EOF
     for com in tri info train jt vit; do
-      if test -x $1/$d/${com}command; then
+      if test -x $ddd/${com}command; then
 	cat <<EOF >>$TAD/test_and_dev_${d}.at
 
-AT_SETUP([${d}: ${com}command])
-AT_SKIP_IF([test ! -x \$TEST_AND_DEV/${d}/${com}command])
-AT_CHECK([(cd \$TEST_AND_DEV/${d} && ulimit -v 8000000 && ulimit -t 120 && ./${com}command)], [], [ignore], [ignore])
+AT_SETUP([${dd}: ${com}command])
+AT_SKIP_IF([test ! -x \$TEST_AND_DEV/$dd/${com}command])
+AT_CHECK([(cd \$TEST_AND_DEV/$dd && ulimit -v 8000000 && \
+           ulimit -t 120 && ./${com}command)], [], [ignore], [ignore])
 AT_CLEANUP
 EOF
       fi
