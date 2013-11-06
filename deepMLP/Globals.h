9e86b5bbc793098b3e205f43da84fb3bc7c1b940
Galen Andrew <galen@cs.washington.edu>
2013-11-05 17:08:02 -0800

    Added comments to Globals.h refs #435

--- deepMLP/Globals.h
+++ deepMLP/Globals.h
@@ -1,5 +1,10 @@
 #pragma once
 
+/*********
+This file, Globals.h, contains some basic functions that are used all
+over the NN training code.
+ *********/
+
 #include <emmintrin.h>
 #include <xmmintrin.h>
 #include <limits>
@@ -10,6 +15,7 @@
 
 using namespace std;
 
+// swap the values of a and b
 template<class T>
 void Swap(T a, T b) {
   T t = a;
@@ -17,6 +23,7 @@ void Swap(T a, T b) {
   b = t;
 }
 
+// macro defining hard assertion: aborts program if condition fails
 #define ASSERT(TST) ( (TST) ? (void)0 : (std::cerr << __FILE__ "(" << __LINE__	<< "): Assertion failed " #TST << std::endl,abort()) )
 
 static const double INFTY = std::numeric_limits<double>::infinity();
@@ -35,6 +42,9 @@ static bool IsInf(double x) { return std::isinf(x); }
 
 static bool IsDangerous(double x) { return IsNaN(x) || IsInf(x); }
 
+// Computes the logsum of x and y, that is
+// log(exp(x) + exp(y))
+// in an efficient and numerical stable way
 static double LogSum(double x, double y) {
   double d = x - y;
   if (d < -30) return y;
@@ -43,18 +53,27 @@ static double LogSum(double x, double y) {
   else return y + log(1.0 + exp(d));
 }
 
+// Computes the logistic sigmoid of x
+// 1/(1+exp(-x))
+// in an efficient and numerical stable way
 static double Logistic(double x) {
   if (x < -30) return 0;
   else if (x > 30) return 1;
   else return 1.0 / (1.0 + exp(-x));
 }
 
+// Computes the negative log of the logistic sigmoid
+// log(1+exp(-x))
+// in an efficient and numerical stable way
 static double LogLoss(double x) {
   if (x < -30) return -x;
   else if (x > 30) return 0;
   else return log(1 + exp(-x));
 }
 
+// Assuming the class C implements a method
+// void Serialize(std::ofstream)
+// Serialize c to a file with the given path
 template<class C>
 void Serialize(const C & c, const string & filename) {
   ofstream outStream(filename, ios::out|ios::binary);
@@ -68,6 +87,9 @@ void Serialize(const C & c, const string & filename) {
   outStream.close();
 }
 
+// Assuming the class C implements a method
+// void Deserialize(std::ofstream)
+// Deserialize c from a file with the given path
 template<class C>
 void Deserialize(C & c, const string & filename) {
   ifstream inStream(filename, ios::in|ios::binary);
