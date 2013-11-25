59417fda1b719de3e2dc1f588a3e961e298987a4
Richard Rogers <rprogers@uw.edu>
2013-11-25 11:31:24 -0800

    make dense layer initialization the default; refs #435

--- deepMLP/DBN.cc
+++ deepMLP/DBN.cc
@@ -3,6 +3,6 @@
 
 bool     DBN::resumeTraining  = false;
 bool     DBN::checkSignal     = false;
-bool     DBN::sparseInitLayer = true;
+bool     DBN::sparseInitLayer = false;
 unsigned DBN::nnChunkSize     = 4;
 
