bfbc565eca8600e758dee647a59ebab49883d33f
Richard Rogers <rprogers@uw.edu>
2015-04-10 23:26:24 -0700

    more work on refactor architecture

--- tksrc/GMTK_InferenceTasks.h
+++ tksrc/GMTK_InferenceTasks.h
@@ -2,73 +2,68 @@
 // InferenceTask subclasses handle inference over a series of sections. The 
 // Sections are responsible for doing inference within themselves via whatever
 // inference algorithm the Section subclass implements. The InferenceTask 
-// classes just handle the messages between Sections via instances of Separator.
+// classes just handle the messages between Sections via instances of InterfaceSeparator.
 
 // Specific sequence inference algorithms multiply inherit the InferenceTask
 // APIs they support. Not all sequence inference algorithms support every 
 // inference task, e.g., Island can't do online filtering/smoothing.
 
 
+// consider ditching the InferenceTask class, making all the *Task classes
+// pure abstract bases for the section inference algorithms, which could
+// then take the section_inference_alg as a template parameter...
+
 class InferenceTask {
  public:
-  InferenceTask(SectionInferenceAlgorithm &inference_alg) : 
-    inference_alg(inference_alg) {}
+  InferenceTask(SectionInferenceAlgorithm &section_inference_alg) : 
+    section_inference_alg(section_inference_alg) {}
 
   virtual ~InferenceTask() {}
 
  protected:
 
-  SectionInferenceAlgorithm &inference_alg;
+  SectionInferenceAlgorithm &section_inference_alg;
 };
 
 
 class ProbEvidenceTask : public virtual InferenceTask {
  public:
-  ProbEvidenceTask(SectionInferenceAlgorithm &inference_alg) : 
-    InferenceTask(inference_alg) {}
+  ProbEvidenceTask(SectionInferenceAlgorithm &section_inference_alg) : 
+    InferenceTask(section_inference_alg) {}
 
   virtual ~ProbEvidenceTask() {}
 
   // compute log P(X_{0:T-1}), forward pass only, O(1) memory
   virtual logpr probEvidence() {
-    auto_ptr<Section> P(inference_alg.getSection(0)); // Section factory method
-
-    // Hmm... not sure about the auto_ptr. If the Sections can be
-    // backed by arrays, the Section ownership should stay with
-    // the SectionInferenceAlgorithm. Either the Section dtor needs to
-    // do the right thing, or perhaps add Section::releaseSection()
-    // method that clients have to remember to call
-
-    Separator msg = P->computeForwardSeparator();
-    for (int t=1; t < T-1; ++t) {
-      auto_ptr<Section> C_t(inference_alg.getSection(t));
-      C_t->receiveForwardSeparator(msg);
-      msg = C_t->computeForwardSeparator();
+    InterfaceSeparator msg = section_inference_alg.computeForwardInterfaceSeparator(0);
+    unsigned t;
+    for (t=1; t < T-1; ++t) {
+      section_inference_alg.receiveForwardInterfaceSeparator(t, msg);
+      msg = section_inference_alg.computeForwardInterfaceSeparator(t);
     }
-    auto_ptr<Section> E(inference_alg.getSection(T-1));
-    E->receiveForwardSeparator(msg);
-    (void) E->computeForwardSeparator(); // E's gather into root
-    printf("ProbE = %f\n", E->probEvidence());
+    section_inference_alg.receiveForwardInterfaceSeparator(t, msg);
+    section_inference_alg.computeForwardInterfaceSeparator(t); // E's gather into root
+    printf("ProbE = %f\n", section_inference_alg.probEvidence(t));
   }
 };
 
 
 class ViterbiTask : public virtual InferenceTask {
  public:
-  ViterbiTask(SectionInferenceAlgorithm &inference_alg) : 
-    InferenceTask(inference_alg) {}
+  ViterbiTask(SectionInferenceAlgorithm &section_inference_alg) : 
+    InferenceTask(section_inference_alg) {}
 
   virtual ~ViterbiTask() {}
 
   // compute argmax P(Q_{0:T-1} | X_{0:T-1})
-  virtual logpr viterbi(unsigned nBest=1) = 0;
+  virtual logpr viterbi(unsigned kBest=1) = 0; // returns log prob of MPE
 };
 
 
 class ForwardBackwardTask : public virtual InferenceTask {
  public:
-  ForwardBackwardTask(SectionInferenceAlgorithm &inference_alg) :
-    InferenceTask(inference_alg) {}
+  ForwardBackwardTask(SectionInferenceAlgorithm &section_inference_alg) :
+    InferenceTask(section_inference_alg) {}
 
   virtual ~ForwardBackwardTask() {}
 
@@ -77,15 +72,16 @@ class ForwardBackwardTask : public virtual InferenceTask {
 };
 
 
+// Note - this uses StreamSource rather than FileSource
 class OnlineSmoothingTask : public virtual InferenceTask {
  public:
-  OnlineSmoothingTask(SectionInferenceAlgorithm &inference_alg) : 
-    InferenceTask(inference_alg) {}
+  OnlineSmoothingTask(SectionInferenceAlgorithm &section_inference_alg) : 
+    InferenceTask(section_inference_alg) {}
 
   virtual ~OnlineSmoothingTask() {}
 
   // compute argmax P(Q_t | X_{0:t+\tau})
-  virtual logpr onlineSmoothing(unsigned futureSections=0);
+  virtual logpr onlineSmoothing(unsigned futureSections=0, unsigned kBest=1);
 };
 
 
@@ -99,12 +95,24 @@ class IslandInference : public ProbEvidenceTask,
                         public ForwardBackwardTask
 {...};
 
+
 class ArchipelagosInference : public ProbEvidenceTask, 
                               public ViterbiTask, 
                               public ForwardBackwardTask
 {...};
 
-class OnlineInference : public OnlineSmoothingTask {...}
+
+class OnlineInference :  public ProbEvidenceTask, 
+                         public OnlineSmoothingTask
+{...};
+
+
+// cuurent "standard" inference
+class ???Inference : public ProbEvidenceTask,
+                     public ViterbiTask,
+                     public ForwardBackwardTask
+{...};
+
 
 // O(Tn) memory
 class OnlyKeepSeperatorInference : public ProbEvidenceTask, 
@@ -112,7 +120,10 @@ class OnlyKeepSeperatorInference : public ProbEvidenceTask,
                                    public ForwardBackwardTask
 {...};
 
-// assumes no edges between sections: no receive{For,Back}wardSeparator() calls
+
+// Assumes no edges between sections: no receive{For,Back}wardInterfaceSeparator() calls.
+// The other InferenceTask implelentations should be able to handle static models
+// (they'll just pass around empty InterfaceSeparators), so this isn't strictly necessary.
 class StaticInference : public ProbEvidenceTask,
                         public ViterbiTask,
                         public forwardBackwardTask
@@ -129,23 +140,54 @@ class SectionInferenceAlgorithm {
 
   virtual ~SectionInferenceAlgorithm() {}
 
-  // Prepare to do inference on segment of length T
-  virtual void unroll(unsigned T) = 0;
+  // Prepare to do inference on segment of length T. Note that this
+  // is the analog of the current JunctionTree::unroll() which sets up the
+  // PartitionStructureArray for at most  P' C' C' E' (4 sections, so
+  // O(1) memory), and the PartitionTableArray for between 0 and T
+  // sections depending on ZeroTable, ShortTable, or LongTable. 
+  // JT::unroll() also allocates O(T) memory (optionally memory mapped)
+  // to store the the Viterbi values if they aren't being written to
+  // a file.
 
-  // factory method returning a C_t Section of the appropriate subclass
-  virtual Section *getSection(unsigned section_number) = 0;
-};
+  // Since we now have the ability to write Viterbi values to files, do
+  // we want to drop the ability to store them in memory? This would
+  // break things, as a Viterbi file would then be a required argument.
+  // it would also be slower for applications that do fit in memory
+  // because of the higher overhead of file I/O operations.
+
+  virtual unsigned unroll(unsigned T) = 0; // returns number of usable frames
 
 
-// Section subclasses can manage their own message ordering w/in a section
+  // compute forward message for C'_t -> C'_{t+1} (aka gather into root)
+  virtual InterfaceSeparator computeForwardInterfaceSeparator(unsigned t) = 0; 
 
-class Section {
+  // recieve forward message for C'_{t-1} -> C'_t (sendForwardsCrossPartitions)
+  virtual void receiveForwardInterfaceSeparator(unsigned t, InterfaceSeparator const &msg) = 0;
 
+
+  // compute backward message for C'_{t-1} <- C'_t (aka scatter out of root)
+  virtual InterfaceSeparator computeBackwardsInterfaceSeparator(unsigned t) = 0;
+
+  // recieve backward message for C'_t <- C'_{t+1} (sendBackwardCrossPartitions)
+  virtual void receiveBackwardInterfaceSeparator(unsigned t, InterfaceSeparator const &msg) = 0;
+
+
+  // return P(Q_t | X_{?}), where ? depends on the messages C_t has seen so far:
+  //        P(Q_t | X_{0:t}) in the forward pass
+  //        P(Q_t | X_{0:T-1}) in a (full) backward pass
+  //        P(Q_t | X_{0:t+\tau}) in a smoothing backward pass
+  virtual logpr probEvidence(unsigned t) = 0;
+
+
+  // Section subclasses can manage their own message ordering w/in a section
   // read/write the section's inference plan (JT, msg orders, etc)
   void readInferencePlan(iDataStreamFile *f);
   void writeInferencePlan(ioDataStreamFile *f);
+};
 
-  // in the following, this is C_t
+
+
+class Section {
 
   /*
 
@@ -229,39 +271,21 @@ receiveFromRightNeighborsLeftInterfaceSeparator()
 
 
 
-
-  // compute forward message for C'_t -> C'_{t+1} (aka gather into root)
-  virtual Separator computeForwardSeparator() = 0; 
-
-  // recieve forward message for C'_{t-1} -> C'_t (sendForwardsCrossPartitions)
-  virtual void receiveForwardSeparator(Separator msg) = 0;
-
-  // compute backward message for C'_{t-1} <- C'_t (aka scatter out of root)
-  virtual Separator computeBackwardsSeparator() = 0;
-
-  // recieve backward message for C'_t <- C'_{t+1} (sendBackwardCrossPartitions)
-  virtual void receiveBackwardSeparator(Separator msg) = 0;
-
-  // return P(Q_t | X_{?}), where ? depends on the messages C_t has seen so far:
-  //        P(Q_t | X_{0:t}) in the forward pass
-  //        P(Q_t | X_{0:T-1}) in a (full) backward pass
-  //        P(Q_t | X_{0:t+\tau}) in a smoothing backward pass
-  virtual logpr probEvidence() = 0;
 };
 
 
 
 // Initially this might be just a single interface clique as in the current
-// inference implementation. Eventually, it could be a general junction forest.
-// Section subclasses must know how to "send" and "recieve" Separators in each
+// inference implementation. Eventually, it could be a junction forest or general graph.
+// Section subclasses must know how to "send" and "recieve" InterfaceSeparators in each
 // direction (CE/DE).
 
-class Separator {
+class InterfaceSeparator {
  public:
   
   // read/write the factored interface spec
-  void readInterface(iDataStreamFile *f);
-  void writeInterface(ioDataStreamFile *f);
+  virtual static void readInterface(iDataStreamFile *f);
+  virtual static void writeInterface(ioDataStreamFile *f);
   
   // methods to make structure and values available to Section inference alg
 };
